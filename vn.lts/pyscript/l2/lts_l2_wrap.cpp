fffdc06d92e29fa74779630d90a7e094f35539b3
WOLF <xiaoyou.chen@hotmail.com>
2015-03-30 11:18:38 +0800

    添加对LTS柜台L2行情API的支持

---
+++ vn.lts/pyscript/l2/lts_l2_wrap.cpp
@@ -0,0 +1,182 @@
+virtual void onRspError(dict error, int id, bool last)
+{
+	 PyLock lock;
+
+	try
+	{
+		this->get_override("onRspError")(error, id, last);
+	}
+	catch (error_already_set const &)
+	{
+		PyErr_Print();
+	}
+};
+
+virtual void onRspUserLogin(dict data, dict error, int id, bool last)
+{
+	 PyLock lock;
+
+	try
+	{
+		this->get_override("onRspUserLogin")(data, error, id, last);
+	}
+	catch (error_already_set const &)
+	{
+		PyErr_Print();
+	}
+};
+
+virtual void onRspUserLogout(dict data, dict error, int id, bool last)
+{
+	 PyLock lock;
+
+	try
+	{
+		this->get_override("onRspUserLogout")(data, error, id, last);
+	}
+	catch (error_already_set const &)
+	{
+		PyErr_Print();
+	}
+};
+
+virtual void onRspSubL2MarketData(dict data, dict error, int id, bool last)
+{
+	 PyLock lock;
+
+	try
+	{
+		this->get_override("onRspSubL2MarketData")(data, error, id, last);
+	}
+	catch (error_already_set const &)
+	{
+		PyErr_Print();
+	}
+};
+
+virtual void onRspUnSubL2MarketData(dict data, dict error, int id, bool last)
+{
+	 PyLock lock;
+
+	try
+	{
+		this->get_override("onRspUnSubL2MarketData")(data, error, id, last);
+	}
+	catch (error_already_set const &)
+	{
+		PyErr_Print();
+	}
+};
+
+virtual void onRspSubL2Index(dict data, dict error, int id, bool last)
+{
+	 PyLock lock;
+
+	try
+	{
+		this->get_override("onRspSubL2Index")(data, error, id, last);
+	}
+	catch (error_already_set const &)
+	{
+		PyErr_Print();
+	}
+};
+
+virtual void onRspUnSubL2Index(dict data, dict error, int id, bool last)
+{
+	 PyLock lock;
+
+	try
+	{
+		this->get_override("onRspUnSubL2Index")(data, error, id, last);
+	}
+	catch (error_already_set const &)
+	{
+		PyErr_Print();
+	}
+};
+
+virtual void onRtnL2MarketData(dict data)
+{
+	 PyLock lock;
+
+	try
+	{
+		this->get_override("onRtnL2MarketData")(data);
+	}
+	catch (error_already_set const &)
+	{
+		PyErr_Print();
+	}
+};
+
+virtual void onRtnL2Index(dict data)
+{
+	 PyLock lock;
+
+	try
+	{
+		this->get_override("onRtnL2Index")(data);
+	}
+	catch (error_already_set const &)
+	{
+		PyErr_Print();
+	}
+};
+
+virtual void onRspSubL2OrderAndTrade(dict data, dict error, int id, bool last)
+{
+	 PyLock lock;
+
+	try
+	{
+		this->get_override("onRspSubL2OrderAndTrade")(data, error, id, last);
+	}
+	catch (error_already_set const &)
+	{
+		PyErr_Print();
+	}
+};
+
+virtual void onRspUnSubL2OrderAndTrade(dict data, dict error, int id, bool last)
+{
+	 PyLock lock;
+
+	try
+	{
+		this->get_override("onRspUnSubL2OrderAndTrade")(data, error, id, last);
+	}
+	catch (error_already_set const &)
+	{
+		PyErr_Print();
+	}
+};
+
+virtual void onRtnL2Order(dict data)
+{
+	 PyLock lock;
+
+	try
+	{
+		this->get_override("onRtnL2Order")(data);
+	}
+	catch (error_already_set const &)
+	{
+		PyErr_Print();
+	}
+};
+
+virtual void onRtnL2Trade(dict data)
+{
+	 PyLock lock;
+
+	try
+	{
+		this->get_override("onRtnL2Trade")(data);
+	}
+	catch (error_already_set const &)
+	{
+		PyErr_Print();
+	}
+};
+
