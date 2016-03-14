faef67efdfb00637e8d53a5bfe336d6f7e7ab4aa
Paolo <freeitaly@gmail.com>
2016-03-14 17:30:36 +0800

    update

---
+++ vn.trader/vtMain_2003.py
@@ -0,0 +1,26 @@
+# encoding: UTF-8
+
+import sys
+import ctypes
+
+from vtEngine import MainEngine
+from uiMainWindow import *
+
+#----------------------------------------------------------------------
+def main():
+    """主程序入口"""
+    # 设置底部任务栏图标，win7以下请注释掉
+    # ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('vn.py demo')
+
+    app = QtGui.QApplication(sys.argv)
+    app.setWindowIcon(QtGui.QIcon('vnpy.ico'))
+    app.setFont(BASIC_FONT)
+
+    mainEngine = MainEngine()
+    mainWindow = MainWindow(mainEngine, mainEngine.eventEngine)
+    mainWindow.showMaximized()
+
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
