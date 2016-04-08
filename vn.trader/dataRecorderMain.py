eb8233a073a48ad3c0e0ce13c3bf5e11b46a63de
RT Server <freeitaly@gmail.com>
2016-04-08 22:44:19 +0800

    添加独立的期货tick订阅@存储模块-2

--- vn.trader/dataRecorderMain.py
+++ vn.trader/dataRecorderMain.py
@@ -1,17 +1,14 @@
 # encoding: UTF-8
-
-import sys
 import ctypes
-
+from uiDataRecorderMainWindow import *
 from vtEngine import MainEngine
-from uiMainWindow import *
 
 #----------------------------------------------------------------------
 def main():
     """主程序入口"""
     # 设置底部任务栏图标，win7以下请注释掉
     try:
-        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('vn.py demo')
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('vnpy')
     except:
         pass
     app = QtGui.QApplication(sys.argv)
@@ -19,10 +16,11 @@ def main():
     app.setFont(BASIC_FONT)
 
     mainEngine = MainEngine()
+
     mainWindow = MainWindow(mainEngine, mainEngine.eventEngine)
-    mainWindow.showMaximized()
+    mainWindow.show()
 
     sys.exit(app.exec_())
-
 if __name__ == '__main__':
     main()
+
