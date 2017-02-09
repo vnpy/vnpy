b241e0a1196753b2f04ead88c90c8122fcd87191
akeyong <akeyong@163.com>
2017-02-09 17:22:49 +0800

    Update vtEngine.py

--- vn.trader/vtEngine.py
+++ vn.trader/vtEngine.py
@@ -25,7 +25,7 @@ class MainEngine(object):
         """Constructor"""
         # 记录今日日期
         self.todayDate = datetime.now().strftime('%Y%m%d')
-        # just for test git1
+         
         # 创建事件引擎
         self.eventEngine = EventEngine2()
         self.eventEngine.start()
