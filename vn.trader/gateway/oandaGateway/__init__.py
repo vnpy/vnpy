9f6ea1fd5c04d040a88e8d2cdcfb910bba64d04b
chenxy123 <xiaoyou.chen@hotmail.com>
2017-03-18 12:19:51 +0800

    1. ctaAlgo模块改名ctaStrategy 2. 将vn.trader下的所有接口统一放到了一个单独的gateway文件夹中，并实现自动识别和加载

---
+++ vn.trader/gateway/oandaGateway/__init__.py
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+
+import vtConstant
+from oandaGateway import OandaGateway as gateway
+
+gatewayName = 'OANDA'
+gatewayDisplayName = gatewayName
+gatewayType = vtConstant.GATEWAYTYPE_INTERNATIONAL
+gatewayQryEnabled = True
