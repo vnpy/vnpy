f2e15cba651f157d222cf83faa0dbdab086d4b8b
chenxy123 <xiaoyou.chen@hotmail.com>
2017-03-20 22:51:43 +0800

    完成CTA策略模块的翻译

---
+++ vn.trader/ctaStrategy/language/English/text.py
@@ -0,0 +1,16 @@
+# encoding: UTF-8
+
+INIT = u'Init'
+START = u'Start'
+STOP = u'Stop'
+
+CTA_ENGINE_STARTED = u'CTA engine started.'
+
+CTA_STRATEGY = u'CTA Strategy'
+LOAD_STRATEGY = u'Load Strategy'
+INIT_ALL = u'Init All'
+START_ALL = u'Start All'
+STOP_ALL = u'Stop All'
+SAVE_POSITION_DATA = u'Save Position Data'
+
+STRATEGY_LOADED = u'Strategy loaded.'
