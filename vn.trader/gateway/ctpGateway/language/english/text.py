bfd82025291a86388dbeb593afbc9698be1e9b0f
chenxy123 <xiaoyou.chen@hotmail.com>
2017-04-19 21:36:27 +0800

    重新添加语言文件夹

---
+++ vn.trader/gateway/ctpGateway/language/english/text.py
@@ -0,0 +1,18 @@
+# encoding: UTF-8
+
+LOADING_ERROR = u'Error occurred when loading the config file, please check.'
+CONFIG_KEY_MISSING = u'Key missing in the config file, please check.'
+
+DATA_SERVER_CONNECTED = u'Data server connected.'
+DATA_SERVER_DISCONNECTED = u'Data server disconnected'
+DATA_SERVER_LOGIN = u'Data server login completed.'
+DATA_SERVER_LOGOUT = u'Data server logout completed.'
+
+TRADING_SERVER_CONNECTED = u'Trading server connected.'
+TRADING_SERVER_DISCONNECTED = u'Trading server disconnected.'
+TRADING_SERVER_AUTHENTICATED = u'Trading server authenticated.' 
+TRADING_SERVER_LOGIN = u'Trading server login completed.'
+TRADING_SERVER_LOGOUT = u'Trading server logout completed.'
+
+SETTLEMENT_INFO_CONFIRMED = u'Settlement info confirmed.'
+CONTRACT_DATA_RECEIVED = u'Contract data received.'
