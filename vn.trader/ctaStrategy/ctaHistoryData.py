dd73767f7a939aa3e09df33a20968613bfb65721
Hardy <hardy0wu@gmail.com>
2017-04-21 02:22:42 +0800

    fix(stragety): load env vars in backtesting scripts

--- vn.trader/ctaStrategy/ctaHistoryData.py
+++ vn.trader/ctaStrategy/ctaHistoryData.py
@@ -491,6 +491,9 @@ if __name__ == '__main__':
     #e.downloadEquityDailyBarts('000001')
     
     import os
+    from dotenv import load_dotenv, find_dotenv
+    
+    load_dotenv(find_dotenv())
     # 这里将项目中包含的股指日内分钟线csv导入MongoDB，作者电脑耗时大约3分钟
     loadMcCsv( os.path.join(os.path.dirname(__file__), 'IF0000_1min.csv'), MINUTE_DB_NAME, 'IF0000')
     #导入通达信历史分钟数据
--- vn.trader/ctaStrategy/strategy/strategyAtrRsi.py
+++ vn.trader/ctaStrategy/strategy/strategyAtrRsi.py
@@ -241,7 +241,11 @@ if __name__ == '__main__':
     # 导入PyQt4的包是为了保证matplotlib使用PyQt4而不是PySide，防止初始化出错
     from ..ctaBacktesting import *
     from PyQt4 import QtCore, QtGui
+    from dotenv import load_dotenv, find_dotenv
     
+    # 加载变量
+    load_dotenv(find_dotenv())
+
     # 创建回测引擎
     engine = BacktestingEngine()
     
