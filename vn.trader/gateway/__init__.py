ee36ddea45d874cf543d3c869bfffd98c5de3ae9
lamter <lamter.fu@gmail.com>
2017-04-26 22:28:15 +0800

    - change : 添加加载交易接口错误的log

--- vn.trader/gateway/__init__.py
+++ vn.trader/gateway/__init__.py
@@ -18,16 +18,19 @@ path = os.path.abspath(os.path.dirname(__file__))
 for root, subdirs, files in os.walk(path):
     if path != root:
         continue
-    
+
     for foldername in subdirs:
         # 接口目录名中必须含有Gateway
         if 'Gateway' in foldername:
             # 模块名称需要上前缀
             moduleName = 'gateway.' + foldername
-            
+
             try:
                 # 使用importlib动态载入模块，并保存到字典中
                 module = importlib.import_module(moduleName)
                 GATEWAY_DICT[module.gatewayName] = module
+                print('load %s' % moduleName)
+            except ImportError:
+                print('module %s not find ' % moduleName)
             except:
                 traceback.print_exc()
