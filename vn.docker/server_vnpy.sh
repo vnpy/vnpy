2c5abc4d90c8efa87896e223e173353874061ad9
lamter <lamter.fu@gmail.com>
2017-04-26 22:23:47 +0800

    - change : Docker启动命令添加映射VT_setting.json 的参数

--- vn.docker/server_vnpy.sh
+++ vn.docker/server_vnpy.sh
@@ -3,6 +3,7 @@
 cd ..
 docker run --name vnpy --rm \
     -v `pwd`:/srv/vnpy \
+    -v `pwd`/../vn.trader/VT_setting.json:/etc/vnpy/VT_setting.json \
     -p 2014:2014 -p 602:602 \
     vnpy:latest \
-    python /srv/vnpy/vn.trader/vtServer.py
+    python /srv/vnpy/vn.trader/vtServer.py --VT_setting /etc/vnpy/VT_setting.json
