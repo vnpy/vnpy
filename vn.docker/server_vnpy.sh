45d4301110a4fb89b6f34b532e41eddecd1e2183
lamter <lamter.fu@gmail.com>
2017-04-27 13:07:30 +0800

    - change : Dockfile 不设置入口点ENTRYPOINT ，否则不能使用 -it 交互模式

--- vn.docker/server_vnpy.sh
+++ vn.docker/server_vnpy.sh
@@ -6,4 +6,4 @@ docker run --name vnpy --rm \
     -v `pwd`/vn.trader/tmp/VT_setting.json:/etc/vnpy/VT_setting.json \
     -p 2014:2014 -p 602:602 \
     vnpy:latest \
-    --VT_setting /etc/vnpy/VT_setting.json
+    python /srv/vnpy/vn.trader/vtServer.py --VT_setting /etc/vnpy/VT_setting.json
