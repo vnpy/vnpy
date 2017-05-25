92b17693b708867b776dce8e1f9b3cccf242e7fb
ipqhjjybj <250657661@qq.com>
2017-05-25 20:48:54 +0800

    发现只删除ctaStrategy里的text会有问题，最好是合并的方式
    发现只删除ctaStrategy里的text会有问题，最好是合并的方式。
    
    这样既能解决单独执行 ctaBacktesting问题，也能解决外层调用的显示问题。

--- vn.trader/language/english/text.py
+++ vn.trader/language/english/text.py
@@ -2,6 +2,26 @@
 
 SAVE_DATA = 'Save Data'
 
+#####################################################
+# merge ! 
+INIT = u'Init'
+START = u'Start'
+STOP = u'Stop'
+
+CTA_ENGINE_STARTED = u'CTA engine started.'
+
+CTA_STRATEGY = u'CTA Strategy'
+LOAD_STRATEGY = u'Load Strategy'
+INIT_ALL = u'Init All'
+START_ALL = u'Start All'
+STOP_ALL = u'Stop All'
+SAVE_POSITION_DATA = u'Save Position Data'
+
+STRATEGY_LOADED = u'Strategy loaded.'
+
+SAVE_POSITION_QUESTION = u'Do you want to save strategy position data into database?'
+
+############################################################ 
 CONTRACT_SYMBOL = 'Symbol'
 CONTRACT_NAME = 'Name'
 LAST_PRICE = 'Last'
