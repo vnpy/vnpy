2189cdef62e67b04c3146832177e5909fe6256b5
vn.py <vn.py@foxmail.com>
2017-07-07 17:59:31 +0800

    Examples中新增上海中期历史行情下载服务

--- vnpy/data/shcifco/test.py
+++ vnpy/data/shcifco/test.py
@@ -4,9 +4,9 @@ from vnshcifco import ShcifcoApi, PERIOD_1MIN
 
 
 if __name__ == "__main__":
-    ip = '101.231.179.199'
-    port  = '10102'
-    token = 'testd2cda34b2d317779e812eb84ee4224a6_qpweqf1'
+    ip = '180.169.126.123'
+    port  = '45065'
+    token = '请联系上海中期申请'
     symbol = 'cu1709'
     
     # 创建API对象
@@ -22,6 +22,6 @@ if __name__ == "__main__":
     print api.getLastBar(symbol)
     
     # 获取历史分钟线
-    print api.getHisBar(symbol, 502, period=PERIOD_1MIN)
+    print api.getHisBar(symbol, 500, period=PERIOD_1MIN)
     
     
