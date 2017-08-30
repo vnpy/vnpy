a8c0ff957c44e898024b38ba8f5e230ee5a5d4d9
vn.py <vn.py@foxmail.com>
2017-08-30 17:31:25 +0800

    新增VnTrader的xtpGateway接入

---
+++ vnpy/trader/gateway/xtpGateway/__init__.py
@@ -0,0 +1,10 @@
+# encoding: UTF-8
+
+from vnpy.trader import vtConstant
+from xtpGateway import XtpGateway
+
+gatewayClass = XtpGateway
+gatewayName = 'XTP'
+gatewayDisplayName = 'XTP'
+gatewayType = vtConstant.GATEWAYTYPE_EQUITY
+gatewayQryEnabled = True
