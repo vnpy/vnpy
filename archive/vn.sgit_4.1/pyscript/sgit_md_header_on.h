6d82c56598c7c4d61703261027107c2c8e33b6ab
vn.py <vn.py@foxmail.com>
2017-08-30 21:18:04 +0800

    移除archive目录

--- archive/vn.sgit_4.1/pyscript/sgit_md_header_on.h
+++
@@ -1,8 +0,0 @@
-
-
-virtual void onRspUserLogin(dict data, dict error, int id, bool last) {};
-
-virtual void onRspUserLogout(dict data, dict error, int id, bool last) {};
-
-virtual void onRtnDepthMarketData(dict data) {};
-
