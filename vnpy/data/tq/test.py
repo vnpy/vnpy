4fa897c2b075dbba1e9048f56f269c3178bcb5c0
vn.py <vn.py@foxmail.com>
2017-09-26 09:35:02 +0800

    [Add]新增天勤数据服务TqDataService

--- vnpy/data/tq/test.py
+++ vnpy/data/tq/test.py
@@ -1,5 +1,6 @@
 # encoding: UTF-8
 
+from time import sleep
 
 from vntq import TqApi
 
@@ -28,15 +29,23 @@ def onChart(symbol, seconds):
         serial = api.get_kline_serial(symbol, seconds)
         
     print serial
-
+    
 
 if __name__ == "__main__":
+    symbol = 'IF1710'
+    
     api = TqApi()
     
     api.connect()
     
-    api.subscribe_quote(["RM801"], onQuote)
-    api.subscribe_chart("RM801", 0, 100, onChart)
+    # 订阅Tick推送
+    #api.subscribe_quote([symbol], onQuote)
+    
+    # 订阅Tick图表
+    #api.subscribe_chart(symbol, 0, 100, onChart)
+    
+    # 订阅K线图表
+    api.subscribe_chart(symbol, 60, 1000, onChart)
     
     raw_input()
     
