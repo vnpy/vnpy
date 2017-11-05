eac266524977fe5961f5ce3f57a9e8a1a341a40a
vn.py <vn.py@foxmail.com>
2017-11-05 13:40:56 +0800

    [Add]开始OptionMaster期权交易模块的开发

---
+++ vnpy/trader/app/optionMaster/__init__.py
@@ -0,0 +1,10 @@
+# encoding: UTF-8
+
+#from rmEngine import RmEngine
+#from uiRmWidget import RmEngineManager
+
+appName = 'OptionMaster'
+appDisplayName = u'OptionMaster'
+appEngine = None
+appWidget = None
+appIco = 'rm.ico'
