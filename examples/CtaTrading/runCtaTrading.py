16b004460be06c8892427daa93e765f309644b39
vn.py <vn.py@foxmail.com>
2017-12-01 23:38:54 +0800

    [Fix]Close #562

--- examples/CtaTrading/runCtaTrading.py
+++ examples/CtaTrading/runCtaTrading.py
@@ -40,7 +40,7 @@ def runChildProcess():
     me.connect('CTP')
     le.info(u'连接CTP接口')
     
-    sleep(5)    # 等待CTP接口初始化
+    sleep(10)    # 等待CTP接口初始化
     
     cta = me.getApp(ctaStrategy.appName)
     
