13b0d297a7716290c502405c88257115fb2f441d
vn.py <vn.py@foxmail.com>
2018-01-29 17:39:49 +0800

    [Del]移除DockerTrader

--- docker/dockerTrader/gateway/femasGateway/__init__.py
+++
@@ -1,9 +0,0 @@
-# encoding: UTF-8
-
-import vtConstant
-from femasGateway import FemasGateway as gateway
-
-gatewayName = 'FEMAS'
-gatewayDisplayName = u'飞马'
-gatewayType = vtConstant.GATEWAYTYPE_FUTURES
-gatewayQryEnabled = True
--- docker/dockerTrader/gateway/qdpGateway/__init__.py
+++
@@ -1,9 +0,0 @@
-# encoding: UTF-8
-
-import vtConstant
-from qdpGateway import QdpGateway as gateway
-
-gatewayName = 'QDP'
-gatewayDisplayName = gatewayName
-gatewayType = vtConstant.GATEWAYTYPE_FUTURES
-gatewayQryEnabled = True
