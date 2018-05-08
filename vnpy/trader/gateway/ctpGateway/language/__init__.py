47378876b910377dd3914694454c1722c684edee
msincenselee <incenselee@hotmail.com>
2018-05-08 21:30:46 +0800

    update

--- vnpy/trader/gateway/ctpGateway/language/__init__.py
+++ vnpy/trader/gateway/ctpGateway/language/__init__.py
@@ -1,12 +1,12 @@
 # encoding: UTF-8
 
 import os
-print u'load {0}/*'.format(os.path.dirname(__file__))
+print( u'load {0}/*'.format(os.path.dirname(__file__)))
 
 # 默认设置
-from chinese import text
+from vnpy.trader.gateway.ctpGateway.language.chinese import text
 
 # 是否要使用英文
 from vnpy.trader.vtGlobal import globalSetting
 if globalSetting['language'] == 'english':
-    from english import text
+    from vnpy.trader.gateway.ctpGateway.language.english import text
