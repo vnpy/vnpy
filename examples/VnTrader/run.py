7df8f56e7ff5ad111025ff5e080d0453a58fcd4d
msincenselee <incenselee@hotmail.com>
2018-05-29 00:05:19 +0800

    -

--- examples/VnTrader/run.py
+++ examples/VnTrader/run.py
@@ -23,7 +23,7 @@ from vnpy.trader.uiMainWindow import *
 # 加载底层接口
 from vnpy.trader.gateway import ctpGateway
 # 初始化的接口模块，以及其指定的名称,CTP是模块，value，是该模块下的多个连接配置文件,如 CTP_JR_connect.json    'CTP_Prod', 'CTP_JR', , 'CTP_JK', 'CTP_02'
-init_gateway_names = {'CTP': ['CTP','CTP_YH01', 'CTP_YH02', 'CTP_YH03','CTP_JK']}
+init_gateway_names = {'CTP': ['CTP','CTP_huafu001','CTP_huafu002']}
 
 from vnpy.trader.app import (ctaStrategy, riskManager, spreadTrading)
 
