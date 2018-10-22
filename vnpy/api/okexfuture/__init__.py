b702f77af95838cd80924864aa1525bd31cf3446
nanoric <nanoric@qq.com>
2018-10-21 22:41:30 -0400

    [Mod] OkexFuture改名为OkexFutures [Add] 增加V3版本的OkexRestApi

--- vnpy/api/okexfuture/__init__.py
+++
@@ -1,2 +0,0 @@
-from .OkexFutureApi import OkexFutureRestClient, OkexFutureWebSocketClient, OkexFutureSymbol, OkexFutureContractType, OkexFutureOrder, OkexFutureOrderStatus, OkexFuturePosition, \
-    OkexFuturePositionDetail, OkexFuturePriceType, OkexFutureUserInfo
---
+++ vnpy/api/okexfutures/__init__.py
@@ -0,0 +1,6 @@
+from .OkexFuturesApi import OkexFuturesAccountInfoV3, OkexFuturesContractType, \
+    OkexFuturesContractsInfoV3, OkexFuturesOrder, OkexFuturesOrderDetailV3, \
+    OkexFuturesOrderSentInfoV3, OkexFuturesOrderStatus, OkexFuturesPosition, \
+    OkexFuturesPositionDetail, OkexFuturesPositionInfoV3, OkexFuturesPriceType, \
+    OkexFuturesRestClientV1, OkexFuturesRestClientV3, OkexFuturesSymbol, OkexFuturesUserInfo, \
+    OkexFuturesWebSocketClient
