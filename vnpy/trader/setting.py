b996285ffd8415c7427ca58cd9d2021f5e24d5d3
vn.py <xiaoyou.chen@foxmail.com>
2019-01-08 20:42:47 +0800

    [Add] Add global setting file

---
+++ vnpy/trader/setting.py
@@ -0,0 +1,15 @@
+"""
+Global setting of VN Trader.
+"""
+
+from logging import CRITICAL
+
+SETTINGS = {
+    "font.family": "Arial",
+    "font.size": 12,
+
+    "log.active": True,
+    "log.level": CRITICAL,
+    "log.console": True,
+    "log.file": True
+}
