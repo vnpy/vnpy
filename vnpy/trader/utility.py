f15bb91f29b5462336358ed8a86167d6e0919c56
vn.py <xiaoyou.chen@foxmail.com>
2019-02-17 22:59:41 +0800

    [Mod] change get_path related functions

--- vnpy/trader/utility.py
+++ vnpy/trader/utility.py
@@ -2,7 +2,6 @@
 General utility functions.
 """
 
-import shelve
 import json
 from pathlib import Path
 from typing import Callable
@@ -19,9 +18,7 @@ class Singleton(type):
 
     class A:
         __metaclass__ = Singleton
-
     """
-
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
@@ -30,29 +27,50 @@ class Singleton(type):
             cls._instances[cls] = super(Singleton, cls).__call__(
                 *args, **kwargs
             )
-
         return cls._instances[cls]
 
 
-def get_trader_path():
+def get_path(temp_name: str):
     """
     Get path where trader is running in.
     """
+    cwd = Path.cwd()
+    temp_path = cwd.joinpath(temp_name)
+
+    # If .vntrader folder exists in current working directory,
+    # then use it as trader running path.
+    if temp_path.exists():
+        return cwd, temp_path
+
+    # Otherwise use home path of system.
     home_path = Path.home()
-    return home_path
+    temp_path = home_path.joinpath(temp_name)
 
-
-def get_temp_path(filename: str):
-    """
-    Get path for temp file with filename.
-    """
-    trader_path = get_trader_path()
-    temp_path = trader_path.joinpath(".vntrader")
-
+    # Create .vntrader folder under home path if not exist.
     if not temp_path.exists():
         temp_path.mkdir()
 
-    return temp_path.joinpath(filename)
+    return home_path, temp_path
+
+
+TRADER_PATH, TEMP_PATH = get_path(".vntrader")
+
+
+def get_file_path(filename: str):
+    """
+    Get path for temp file with filename.
+    """
+    return TEMP_PATH.joinpath(filename)
+
+
+def get_folder_path(folder_name: str):
+    """
+    Get path for temp folder with folder name.
+    """
+    folder_path = TEMP_PATH.joinpath(folder_name)
+    if not folder_path.exists():
+        folder_path.mkdir()
+    return folder_path
 
 
 def get_icon_path(filepath: str, ico_name: str):
@@ -64,33 +82,11 @@ def get_icon_path(filepath: str, ico_name: str):
     return str(icon_path)
 
 
-def load_setting(filename: str):
-    """
-    Load setting from shelve file in temp path.
-    """
-    filepath = get_temp_path(filename)
-    f = shelve.open(str(filepath))
-    setting = dict(f)
-    f.close()
-    return setting
-
-
-def save_setting(filename: str, setting: dict):
-    """
-    Save setting into shelve file in temp path.
-    """
-    filepath = get_temp_path(filename)
-    f = shelve.open(str(filepath))
-    for k, v in setting.items():
-        f[k] = v
-    f.close()
-
-
 def load_json(filename: str):
     """
     Load data from json file in temp path.
     """
-    filepath = get_temp_path(filename)
+    filepath = get_file_path(filename)
 
     if filepath.exists():
         with open(filepath, mode='r') as f:
@@ -105,7 +101,7 @@ def save_json(filename: str, data: dict):
     """
     Save data into json file in temp path.
     """
-    filepath = get_temp_path(filename)
+    filepath = get_file_path(filename)
     with open(filepath, mode='w+') as f:
         json.dump(data, f, indent=4)
 
