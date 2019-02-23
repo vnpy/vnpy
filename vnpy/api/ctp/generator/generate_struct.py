61cbe9dc4a8bbd722f05bc01f6ac767155784824
vn.py <xiaoyou.chen@foxmail.com>
2019-02-23 16:26:14 +0800

    [Mod]reformat ctp wrap generator script

--- vnpy/api/ctp/generator/generate_struct.py
+++ vnpy/api/ctp/generator/generate_struct.py
@@ -1,6 +1,7 @@
 """"""
 import importlib
 
+
 class StructGenerator:
     """Struct生成器"""
 
@@ -9,36 +10,36 @@ class StructGenerator:
         self.filename = filename
         self.prefix = prefix
         self.typedefs = {}
-        
+
         self.load_constant()
-        
+
     def load_constant(self):
         """"""
         module_name = f"{self.prefix}_typedef"
         module = importlib.import_module(module_name)
-        
+
         for name in dir(module):
             if "__" not in name:
                 self.typedefs[name] = getattr(module, name)
-        
+
     def run(self):
         """运行生成"""
         self.f_cpp = open(self.filename, "r")
         self.f_struct = open(f"{self.prefix}_struct.py", "w")
-        
+
         for line in self.f_cpp:
             self.process_line(line)
-            
+
         self.f_cpp.close()
         self.f_struct.close()
-        
+
         print("Struct生成成功")
-    
+
     def process_line(self, line: str):
         """处理每行"""
         line = line.replace(";", "")
         line = line.replace("\n", "")
-        
+
         if line.startswith("struct"):
             self.process_declare(line)
         elif line.startswith("{"):
@@ -53,27 +54,27 @@ class StructGenerator:
         words = line.split(" ")
         name = words[1]
         end = "{"
-        
+
         new_line = f"{name} = {end}\n"
         self.f_struct.write(new_line)
-    
+
     def process_start(self, line: str):
         """处理开始"""
         pass
-    
+
     def process_end(self, line: str):
         """处理结束"""
         new_line = "}\n\n"
         self.f_struct.write(new_line)
-    
+
     def process_member(self, line: str):
         """处理成员"""
         words = line.split("\t")
         words = [word for word in words if word]
-        
+
         py_type = self.typedefs[words[0]]
         name = words[1]
-        
+
         new_line = f"    \"{name}\": \"{py_type}\",\n"
         self.f_struct.write(new_line)
 
