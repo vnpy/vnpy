fc83a62ea04302e641b8fc0e943296568f80ecb5
nanoric <nanoric@qq.com>
2019-03-19 01:00:47 -0400

    [Add] install.sh: make & install ta-lib to /usr

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+
+# Get and build ta-lib
+pushd /tmp
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xf ta-lib-0.4.0-src.tar.gz
+cd ta-lib
+./configure --prefix=/usr
+make
+sudo make install
+popd
+
+# old versions of ta-lib imports numpy in setup.py
+pip install numpy
+
 # Install Python Modules
 pip install -r requirements.txt
 
