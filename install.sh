a7a80e0e9ad8bd5afa6f85f41b6405d5dc763208
nanoric <nanoric@qq.com>
2019-04-15 01:26:45 -0400

    [Fix] try  to fix errors

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ pushd /tmp
 wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
 tar -xf ta-lib-0.4.0-src.tar.gz
 cd ta-lib
-./configure --prefix=/usr
+./configure --prefix=/usr/local
 make
 sudo make install
 popd
