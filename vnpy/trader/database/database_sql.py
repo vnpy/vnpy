7487dfd4e7bd3e3f44d31b1275b3c805eb1f0976
xldistance <819873074@qq.com>
2019-05-16 19:07:37 +0800

    删除合约增加interval的判断，防止删除分钟数据连日线数据一起删了
    删除合约增加interval的判断，防止删除分钟数据连日线数据一起删了

--- vnpy/trader/database/database_sql.py
+++ vnpy/trader/database/database_sql.py
@@ -404,6 +404,6 @@ class SqlManager(BaseDatabaseManager):
             return s.to_tick()
         return None
 
-    def clean(self, symbol: str):
-        self.class_bar.delete().where(self.class_bar.symbol == symbol).execute()
+    def clean(self, symbol: str,interval: "Interval"):
+        self.class_bar.delete().where((self.class_bar.symbol == symbol) & (self.class_bar.interval == interval.value)).execute()
         self.class_tick.delete().where(self.class_tick.symbol == symbol).execute()
