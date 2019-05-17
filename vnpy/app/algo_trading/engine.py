eda98b858ba694451c1206e9e4aea952e83da5da
1122455801 <zhangyangffff@gmail.com>
2019-05-17 12:26:55 +0800

    Update engine.py

--- vnpy/app/algo_trading/engine.py
+++ vnpy/app/algo_trading/engine.py
@@ -46,10 +46,14 @@ class AlgoEngine(BaseEngine):
         from .algos.twap_algo import TwapAlgo
         from .algos.iceberg_algo import IcebergAlgo
         from .algos.sniper_algo import SniperAlgo
+        from .algos.stop_algo import StopAlgo
+        from .algos.best_limit_algo import BestLimitAlgo
 
         self.add_algo_template(TwapAlgo)
         self.add_algo_template(IcebergAlgo)
         self.add_algo_template(SniperAlgo)
+        self.add_algo_template(StopAlgo)
+        self.add_algo_template(BestLimitAlgo)
 
     def add_algo_template(self, template: AlgoTemplate):
         """"""
