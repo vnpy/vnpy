fcff39144b809d86b207de40e5c0437271e2958e
nanoric <nanoric@qq.com>
2019-05-21 15:25:39 +0800

    [Mod] make flake8 happy

--- vnpy/api/itap/generate_error_map.py
+++ vnpy/api/itap/generate_error_map.py
@@ -1,7 +1,8 @@
 from typing import Dict
 
-from autocxxpy.core.cxxparser import CxxFileParser
 import black
+
+from autocxxpy.core.cxxparser import CxxFileParser
 
 parser = CxxFileParser(
     files=["iTapAPIError.h",
@@ -16,13 +17,9 @@ for v in result.g.variables.values():
 for v in result.g.namespaces['ITapTrade'].variables.values():
     error_map[v.value] = v.brief_comment
 
-
 result = f'error_map = {repr(error_map)}'
 
 output_file = 'error_codes.py'
 with open(output_file, "wt", encoding='utf-8') as f:
     f.write(result)
 black.main([output_file])
-
-
-
