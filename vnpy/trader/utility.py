b05eb7142e60fd3cb93e6b04e1c7e89746670462
vn.py <xiaoyou.chen@foxmail.com>
2019-06-13 16:56:44 +0800

    [Add] metaclass of singleton

--- vnpy/trader/utility.py
+++ vnpy/trader/utility.py
@@ -445,3 +445,17 @@ def virtual(func: "callable"):
     that can be (re)implemented by subclasses.
     """
     return func
+
+
+class Singleton(type):
+    """
+    Metaclass for creating singleton object.
+    """
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """"""
+        if cls not in cls._instances:
+            cls._instances[cls] = super(
+                Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
