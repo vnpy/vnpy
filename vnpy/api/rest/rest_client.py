af94e1e186a5ff75be7ea7cc6ed1108e74e7dc57
vn.py <xiaoyou.chen@foxmail.com>
2019-06-25 10:37:40 +0800

    [Del] remove unnecessary try...catch

--- vnpy/api/rest/rest_client.py
+++ vnpy/api/rest/rest_client.py
@@ -257,13 +257,10 @@ class RestClient(object):
             )
             request.response = response
             status_code = response.status_code
-            if status_code // 100 == 2:  # 2xx都算成功，尽管交易所都用200
-                try:
-                    jsonBody = response.json()
-                    request.callback(jsonBody, request)
-                    request.status = RequestStatus.success
-                except:
-                    print("not json body from request, check body: ",response.text)
+            if status_code // 100 == 2:  # 2xx codes are all successful
+                json_body = response.json()
+                request.callback(json_body, request)
+                request.status = RequestStatus.success
             else:
                 request.status = RequestStatus.failed
 
