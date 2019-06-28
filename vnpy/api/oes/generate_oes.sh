adc4969746343a32f5ebc06dfdf5b95676e2e884
nanoric <nanoric@qq.com>
2019-06-28 10:30:52 +0800

    [Mod] update oes api: use new autocxxpy and change its code layout

---
+++ vnpy/api/oes/generate_oes.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# autocxxpy version: 0.3.4b1
+python -m autocxxpy \
+    vnoes \
+    oes_api/oes_api.h \
+    mds_api/mds_api.h \
+    -I vnoes/include \
+    -I vnoes/include/oes \
+    --output-dir vnoes/generated_files \
+    --copy-autocxxpy-includes vnoes/include \
+    --pyi-output-dir .\
+    --no-clear-pyi-output-dir \
+    --no-callback-pattern ".*"
+
+#####################################
+# of unsupported functions: 5
+# OesApi_SendBatchOrdersReq (OesApiSessionInfoT * pOrdChannel,const OesOrdReqT *[] ppOrdPtrList,int32 ordCount)
+# OesApi_WaitOnChannelGroup (OesApiChannelGroupT * pChannelGroup,int32 timeoutMs,F_OESAPI_ON_RPT_MSG_T pOnMsgCallback,void * pCallbackParams,OesApiSessionInfoT ** ppFailed)
+# MdsApi_WaitOnTcpChannelGroup (MdsApiChannelGroupT * pChannelGroup,int32 timeoutMs,F_MDSAPI_ONMSG_T pOnMsgCallback,void * pCallbackParams,MdsApiSessionInfoT ** ppFailed)
+# MdsApi_WaitOnTcpChannelGroupCompressible (MdsApiChannelGroupT * pChannelGroup,int32 timeoutMs,F_MDSAPI_ONMSG_T pOnMsgCallback,void * pCallbackParams,MdsApiSessionInfoT ** ppFailed)
+# MdsApi_WaitOnUdpChannelGroup (MdsApiChannelGroupT * pChannelGroup,int32 timeoutMs,F_MDSAPI_ONMSG_T pOnMsgCallback,void * pCallbackParams,MdsApiSessionInfoT ** ppFailed)
+#####################################
