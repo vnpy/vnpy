2713867b07d0de7463523a8c12feea8883b2aa12
StanleyWang <stanley1937@qq.com>
2019-07-19 15:14:07 +0800

    Update runOptimization.py
    后面的计算是基于日统计的，日统计中用的是totalNetPnl，没有capital,这里会导致结果为0，应改为 'totalNetPnl'才正常。

--- examples/CtaBacktesting/runOptimization.py
+++ examples/CtaBacktesting/runOptimization.py
@@ -34,7 +34,6 @@ if __name__ == '__main__':
     
     # 跑优化
     setting = OptimizationSetting()                 # 新建一个优化任务设置对象
-    setting.setOptimizeTarget('capital')            # 设置优化排序的目标是策略净盈利
     setting.addParameter('atrLength', 12, 20, 2)    # 增加第一个优化参数atrLength，起始12，结束20，步进2
     setting.addParameter('atrMa', 20, 30, 5)        # 增加第二个优化参数atrMa，起始20，结束30，步进5
     setting.addParameter('rsiLength', 5)            # 增加一个固定数值的参数
