0df4204d1e67e6cb1620c9d0bbf4f1f338af1108
vn.py <xiaoyou.chen@foxmail.com>
2019-07-19 22:04:21 +0800

    [Mod] upgrade to ctp mini 1.4 api

--- examples/vn_trader/run.py
+++ examples/vn_trader/run.py
@@ -10,8 +10,8 @@ from vnpy.trader.ui import MainWindow, create_qapp
 # from vnpy.gateway.ib import IbGateway
 # from vnpy.gateway.ctp import CtpGateway
 # from vnpy.gateway.ctptest import CtptestGateway
-# from vnpy.gateway.mini import MiniGateway
-from vnpy.gateway.minitest import MinitestGateway
+from vnpy.gateway.mini import MiniGateway
+# from vnpy.gateway.minitest import MinitestGateway
 # from vnpy.gateway.femas import FemasGateway
 # from vnpy.gateway.tiger import TigerGateway
 # from vnpy.gateway.oes import OesGateway
@@ -47,8 +47,8 @@ def main():
     # main_engine.add_gateway(BinanceGateway)
     # main_engine.add_gateway(CtpGateway)
     # main_engine.add_gateway(CtptestGateway)
-    # main_engine.add_gateway(MiniGateway)
-    main_engine.add_gateway(MinitestGateway)
+    main_engine.add_gateway(MiniGateway)
+    # main_engine.add_gateway(MinitestGateway)
     # main_engine.add_gateway(FemasGateway)
     # main_engine.add_gateway(IbGateway)
     # main_engine.add_gateway(FutuGateway)
