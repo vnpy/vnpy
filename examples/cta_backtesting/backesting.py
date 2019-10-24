ae310288162c51f5372e02e22a6a8cc14444c981
NiuBi <erikgqp8645@gmail.com>
2019-10-24 16:29:46 +0800

    修改回测的策略

--- examples/cta_backtesting/backesting.py
+++ examples/cta_backtesting/backesting.py
@@ -1,31 +1,33 @@
 from vnpy.app.cta_strategy.backtesting import BacktestingEngine, OptimizationSetting
 from vnpy.app.cta_strategy.strategies.atr_rsi_strategy import AtrRsiStrategy
+from vnpy.app.cta_strategy.strategies.turtle_signal_strategy import TurtleSignalStrategy
 from datetime import datetime
 
 engine = BacktestingEngine()
 engine.set_parameters(
-    vt_symbol="IF88.CFFEX",
+    vt_symbol="BTCUSD.BITFINEX",
     interval="1m",
-    start=datetime(2019, 1, 1),
-    end=datetime(2019, 4, 30),
-    rate=0.3 / 10000,
+    start=datetime(2017, 8, 30),
+    end=datetime(2019, 8, 30),
+    rate=0.3 / 1000,
     slippage=0.2,
     size=300,
     pricetick=0.2,
     capital=1_000_000,
 )
-engine.add_strategy(AtrRsiStrategy, {})
+engine.add_strategy(TurtleSignalStrategy, {})
 
 engine.load_data()
 engine.run_backtesting()
 df = engine.calculate_result()
 engine.calculate_statistics()
-engine.show_chart()
+# exit()
+# engine.show_chart()
 
 
 setting = OptimizationSetting()
-setting.set_target("sharpe_ratio")
-setting.add_parameter("atr_length", 3, 39, 1)
-setting.add_parameter("atr_ma_length", 10, 30, 1)
+setting.set_target("total_return")
+setting.add_parameter("entry_window", 5, 50, 5)
+setting.add_parameter("exit_window", 10, 90, 10)
 
 engine.run_ga_optimization(setting)
