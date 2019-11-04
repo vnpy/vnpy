06ce7074d970b71f6ce52c361c6f251a2ca8792c
Guo <erikguo@126.com>
2019-11-04 23:17:54 +0800

    修改

--- examples/cta_backtesting/backesting.py
+++ examples/cta_backtesting/backesting.py
@@ -1,33 +1,32 @@
-from vnpy.app.cta_strategy.backtesting import BacktestingEngine, OptimizationSetting
+# from vnpy.app.cta_strategy.backtesting import BacktestingEngine, OptimizationSetting
 from vnpy.app.cta_strategy.strategies.atr_rsi_strategy import AtrRsiStrategy
-from vnpy.app.cta_strategy.strategies.turtle_signal_strategy import TurtleSignalStrategy
 from datetime import datetime
-
+from examples.cta_backtesting.turtle_strategy import TurtleStrategy
+from backtesting_yue import BacktestingEngine,OptimizationSetting
 engine = BacktestingEngine()
 engine.set_parameters(
     vt_symbol="BTCUSD.BITFINEX",
     interval="1m",
-    start=datetime(2017, 8, 30),
-    end=datetime(2019, 8, 30),
+    start=datetime(2017, 8, 1),
+    end=datetime(2018, 8, 1),
     rate=0.3 / 1000,
     slippage=0.2,
     size=300,
     pricetick=0.2,
     capital=1_000_000,
 )
-engine.add_strategy(TurtleSignalStrategy, {})
+engine.add_strategy(TurtleStrategy, {})
 
 engine.load_data()
 engine.run_backtesting()
 df = engine.calculate_result()
 engine.calculate_statistics()
-# exit()
-# engine.show_chart()
-
+engine.show_chart()
+exit()
 
 setting = OptimizationSetting()
-setting.set_target("total_return")
-setting.add_parameter("entry_window", 5, 50, 5)
-setting.add_parameter("exit_window", 10, 90, 10)
+setting.set_target("sharpe_ratio")
+setting.add_parameter("atr_length", 3, 39, 1)
+setting.add_parameter("atr_ma_length", 10, 30, 1)
 
 engine.run_ga_optimization(setting)
