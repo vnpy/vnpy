97233ef38ce9a8a391ff985d2830a6d5bcecb932
Guo <erikguo@126.com>
2019-11-04 23:17:54 +0800

    修改

--- examples/cta_backtesting/backesting.py
+++ examples/cta_backtesting/backesting.py
@@ -1,25 +1,21 @@
 # from vnpy.app.cta_strategy.backtesting import BacktestingEngine, OptimizationSetting
+from vnpy.app.cta_strategy.strategies.atr_rsi_strategy import AtrRsiStrategy
 from datetime import datetime
 from examples.cta_backtesting.turtle_strategy import TurtleStrategy
-# from examples.cta_backtesting.bbreak import RBreakStrategy
-from examples.cta_backtesting.rbreaker import RBraekStrategy
-from examples.cta_backtesting.boll_demo_strategy_22 import BollStrategyMultiplier
-from examples.cta_backtesting.guo_boll_channel_strategy import GuoBollChannelStrategy
-from examples.cta_backtesting.basking import BacktestingEngine, OptimizationSetting
-
+from backtesting_yue import BacktestingEngine,OptimizationSetting
 engine = BacktestingEngine()
 engine.set_parameters(
     vt_symbol="BTCUSD.BITFINEX",
     interval="1m",
-    start=datetime(2019, 1, 1),
-    end=datetime(2019, 10, 1),
-    rate=3 / 1000,
+    start=datetime(2017, 8, 1),
+    end=datetime(2018, 8, 1),
+    rate=0.3 / 1000,
     slippage=0.2,
-    size=1,
+    size=300,
     pricetick=0.2,
-    capital=1000,
+    capital=1_000_000,
 )
-engine.add_strategy(GuoBollChannelStrategy, {})
+engine.add_strategy(TurtleStrategy, {})
 
 engine.load_data()
 engine.run_backtesting()
@@ -27,9 +23,10 @@ df = engine.calculate_result()
 engine.calculate_statistics()
 engine.show_chart()
 exit()
+
 setting = OptimizationSetting()
-setting.set_target("return_drawdown_ratio")
-setting.add_parameter("donchian_window", 10, 1000, 100)
-setting.add_parameter("setup_coef", 0.1, 5, 1)
+setting.set_target("sharpe_ratio")
+setting.add_parameter("atr_length", 3, 39, 1)
+setting.add_parameter("atr_ma_length", 10, 30, 1)
 
 engine.run_ga_optimization(setting)
