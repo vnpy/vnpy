998045b7c86d5349581d24e5535b6b2a8bb91771
vn.py <xiaoyou.chen@foxmail.com>
2019-11-07 14:27:33 +0800

    [Mod] filter close position order data from web ui

--- vnpy/gateway/bitmex/bitmex_gateway.py
+++ vnpy/gateway/bitmex/bitmex_gateway.py
@@ -703,9 +703,15 @@ class BitmexWebsocketApi(WebsocketClient):
 
     def on_order(self, d):
         """"""
+        # Filter order data which cannot be processed properly
         if "ordStatus" not in d:
             return
 
+        side = d.get("side", "")
+        if not side:
+            return
+
+        # Update local order data
         sysid = d["orderID"]
         order = self.orders.get(sysid, None)
         if not order:
@@ -714,14 +720,12 @@ class BitmexWebsocketApi(WebsocketClient):
             else:
                 orderid = sysid
 
-            # time = d["timestamp"][11:19]
-
             order = OrderData(
                 symbol=d["symbol"],
                 exchange=Exchange.BITMEX,
                 type=ORDERTYPE_BITMEX2VT[d["ordType"]],
                 orderid=orderid,
-                direction=DIRECTION_BITMEX2VT[d["side"]],
+                direction=DIRECTION_BITMEX2VT[side],
                 price=d["price"],
                 volume=d["orderQty"],
                 time=d["timestamp"][11:19],
