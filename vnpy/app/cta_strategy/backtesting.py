a25864420139d3675d10a4e9174b2b9a88bdfc27
Guo <erikguo@126.com>
2019-11-18 21:25:10 +0800

    XIUGAI

--- vnpy/app/cta_strategy/backtesting.py
+++ vnpy/app/cta_strategy/backtesting.py
@@ -17,7 +17,7 @@ from vnpy.trader.constant import (Direction, Offset, Exchange,
                                   Interval, Status)
 from vnpy.trader.database import database_manager
 from vnpy.trader.object import OrderData, TradeData, BarData, TickData
-from vnpy.trader.utility import round_to, round_to_nan
+from vnpy.trader.utility import round_to
 
 from .base import (
     BacktestingMode,
@@ -918,7 +918,7 @@ class BacktestingEngine:
             lock: bool
     ):
         """"""
-        price = round_to_nan(price, self.pricetick)
+        price = round_to(price, self.pricetick)
         if stop:
             vt_orderid = self.send_stop_order(direction, offset, price, volume)
         else:
