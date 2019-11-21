363feb827889034784ac701911df35a3bef09a48
Guo <erikguo@126.com>
2019-11-21 22:18:04 +0800

    修改

--- tests/change_data/get.py
+++ tests/change_data/get.py
@@ -6,7 +6,6 @@ pd.set_option('display.max_columns', 1000)  # 设置最大列数
 pd.set_option('display.max_colwidth', 1000)  # 最大列宽度
 pd.set_option('expand_frame_repr', False)  # 当列太多时不换行
 
-df = pd.read_csv("C:\\Users\\NiuBi\\OneDrive\\量化\\数据\\db_bar_data.csv",usecols=["datetime","close_price","high_price","open_price","low_price","volume"],parse_dates=["datetime"])
-df =df[["datetime","open_price","high_price","low_price","close_price","volume"]]
+df_json = pd.read_json("C:\\Users\\erikg\\OneDrive\\量化\数据\\db_bar_data.json")
+df = pd.DataFrame(df_json)
 print(df)
-print(df.dtypes)
