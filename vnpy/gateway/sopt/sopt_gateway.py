4eb9bbcd2aa28a4936f239f595ea187ab6a47740
vn.py <xiaoyou.chen@foxmail.com>
2019-12-18 15:36:40 +0800

    [Mod] remove adjust_price function

--- vnpy/gateway/sopt/sopt_gateway.py
+++ vnpy/gateway/sopt/sopt_gateway.py
@@ -304,25 +304,25 @@ class SoptMdApi(MdApi):
             gateway_name=self.gateway_name
         )
 
-        tick.bid_price_2 = adjust_price(data["BidPrice2"])
-        tick.bid_price_3 = adjust_price(data["BidPrice3"])
-        tick.bid_price_4 = adjust_price(data["BidPrice4"])
-        tick.bid_price_5 = adjust_price(data["BidPrice5"])
+        tick.bid_price_2 = data["BidPrice2"]
+        tick.bid_price_3 = data["BidPrice3"]
+        tick.bid_price_4 = data["BidPrice4"]
+        tick.bid_price_5 = data["BidPrice5"]
 
-        tick.ask_price_2 = adjust_price(data["AskPrice2"])
-        tick.ask_price_3 = adjust_price(data["AskPrice3"])
-        tick.ask_price_4 = adjust_price(data["AskPrice4"])
-        tick.ask_price_5 = adjust_price(data["AskPrice5"])
+        tick.ask_price_2 = data["AskPrice2"]
+        tick.ask_price_3 = data["AskPrice3"]
+        tick.ask_price_4 = data["AskPrice4"]
+        tick.ask_price_5 = data["AskPrice5"]
 
-        tick.bid_volume_2 = adjust_price(data["BidVolume2"])
-        tick.bid_volume_3 = adjust_price(data["BidVolume3"])
-        tick.bid_volume_4 = adjust_price(data["BidVolume4"])
-        tick.bid_volume_5 = adjust_price(data["BidVolume5"])
+        tick.bid_volume_2 = data["BidVolume2"]
+        tick.bid_volume_3 = data["BidVolume3"]
+        tick.bid_volume_4 = data["BidVolume4"]
+        tick.bid_volume_5 = data["BidVolume5"]
 
-        tick.ask_volume_2 = adjust_price(data["AskVolume2"])
-        tick.ask_volume_3 = adjust_price(data["AskVolume3"])
-        tick.ask_volume_4 = adjust_price(data["AskVolume4"])
-        tick.ask_volume_5 = adjust_price(data["AskVolume5"])
+        tick.ask_volume_2 = data["AskVolume2"]
+        tick.ask_volume_3 = data["AskVolume3"]
+        tick.ask_volume_4 = data["AskVolume4"]
+        tick.ask_volume_5 = data["AskVolume5"]
 
         self.gateway.on_tick(tick)
 
