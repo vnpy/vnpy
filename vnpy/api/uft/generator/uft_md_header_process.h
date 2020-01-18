9161a434d5c2b87c1b8d02098d4f9242cf4c4c6e
KeKe <zhangyangffff@gmail.com>
2020-01-18 15:59:11 +0800

    Create uft_md_header_process.h

---
+++ vnpy/api/uft/generator/uft_md_header_process.h
@@ -0,0 +1,10 @@
+void processFrontConnected(Task *task);
+
+void processFrontDisconnected(Task *task);
+
+void processRspDepthMarketDataSubscribe(Task *task);
+
+void processRspDepthMarketDataCancel(Task *task);
+
+void processRtnDepthMarketData(Task *task);
+
