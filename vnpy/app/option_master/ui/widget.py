6e6d147b8312020d8aab2324892eacc3f4ab1c1b
vn.py <xiaoyou.chen@foxmail.com>
2020-01-24 11:12:04 +0800

    [Add] ui widget of delta hedging engine

--- vnpy/app/option_master/ui/widget.py
+++ vnpy/app/option_master/ui/widget.py
@@ -5,16 +5,19 @@ from vnpy.trader.engine import MainEngine
 from vnpy.trader.ui import QtWidgets, QtCore, QtGui
 from vnpy.trader.constant import Direction, Offset, OrderType
 from vnpy.trader.object import OrderRequest, ContractData, TickData
+from vnpy.trader.ui.widget import ActiveOrderMonitor, TradeMonitor
 from vnpy.trader.event import EVENT_TICK
 
 from ..base import APP_NAME, EVENT_OPTION_NEW_PORTFOLIO
 from ..engine import OptionEngine, PRICING_MODELS
 from .monitor import (
-    OptionMarketMonitor, OptionGreeksMonitor, MonitorCell
+    OptionMarketMonitor, OptionGreeksMonitor, OptionChainMonitor,
+    MonitorCell
 )
+from .chart import OptionVolatilityChart, ScenarioAnalysisChart
 
 
-class OptionManager(QtWidgets.QWidget):
+class OptionManager(QtWidgets.QMainWindow):
     """"""
     signal_new_portfolio = QtCore.pyqtSignal(Event)
 
@@ -30,6 +33,8 @@ class OptionManager(QtWidgets.QWidget):
 
         self.market_monitor: OptionMarketMonitor = None
         self.greeks_monitor: OptionGreeksMonitor = None
+        self.volatility_chart: OptionVolatilityChart = None
+        self.chain_monitor: OptionChainMonitor = None
 
         self.docks: List[QtWidgets.QDockWidget] = []
 
@@ -40,6 +45,10 @@ class OptionManager(QtWidgets.QWidget):
         """"""
         self.setWindowTitle("OptionMaster")
 
+        self.init_toolbar()
+
+    def init_toolbar(self):
+        """"""
         self.portfolio_combo = QtWidgets.QComboBox()
         self.portfolio_combo.setFixedWidth(150)
         self.update_portfolio_combo()
@@ -47,29 +56,21 @@ class OptionManager(QtWidgets.QWidget):
         self.portfolio_button = QtWidgets.QPushButton("配置")
         self.portfolio_button.clicked.connect(self.open_portfolio_dialog)
 
-        self.market_button = QtWidgets.QPushButton("T型报价")
-        self.greeks_button = QtWidgets.QPushButton("持仓希腊值")
-        self.chain_button = QtWidgets.QPushButton("拟合升贴水")
-        self.manual_button = QtWidgets.QPushButton("快速交易")
+        restore_button = QtWidgets.QPushButton("还原窗口")
+        restore_button.clicked.connect(self.restore_window_setting)
 
-        for button in [
-            self.market_button,
-            self.greeks_button,
-            self.chain_button,
-            self.manual_button
-        ]:
-            button.setEnabled(False)
+        toolbar = QtWidgets.QToolBar(self)
+        toolbar.setObjectName("期权配置")
+        toolbar.setFloatable(False)
+        toolbar.setMovable(False)
 
-        hbox = QtWidgets.QHBoxLayout()
-        hbox.addWidget(QtWidgets.QLabel("期权产品"))
-        hbox.addWidget(self.portfolio_combo)
-        hbox.addWidget(self.portfolio_button)
-        hbox.addWidget(self.market_button)
-        hbox.addWidget(self.greeks_button)
-        hbox.addWidget(self.manual_button)
-        hbox.addWidget(self.chain_button)
+        toolbar.addWidget(QtWidgets.QLabel("期权产品"))
+        toolbar.addWidget(self.portfolio_combo)
+        toolbar.addWidget(self.portfolio_button)
+        toolbar.addSeparator()
+        toolbar.addWidget(restore_button)
 
-        self.setLayout(hbox)
+        self.addToolBar(QtCore.Qt.TopToolBarArea, toolbar)
 
     def register_event(self):
         """"""
@@ -106,34 +107,136 @@ class OptionManager(QtWidgets.QWidget):
             self.portfolio_button.setEnabled(False)
 
             self.init_widgets()
+            self.load_window_setting("custom")
 
     def init_widgets(self):
         """"""
         self.market_monitor = OptionMarketMonitor(self.option_engine, self.portfolio_name)
         self.greeks_monitor = OptionGreeksMonitor(self.option_engine, self.portfolio_name)
+        self.volatility_chart = OptionVolatilityChart(self.option_engine, self.portfolio_name)
+        self.chain_monitor = OptionChainMonitor(self.option_engine, self.portfolio_name)
         self.manual_trader = OptionManualTrader(self.option_engine, self.portfolio_name)
+        self.order_monitor = ActiveOrderMonitor(self.main_engine, self.event_engine)
+        self.trade_monitor = TradeMonitor(self.main_engine, self.event_engine)
+        self.hedge_widget = OptionHedgeWidget(self.option_engine, self.portfolio_name)
+        self.scenario_chart = ScenarioAnalysisChart(self.option_engine, self.portfolio_name)
 
         self.market_monitor.itemDoubleClicked.connect(self.manual_trader.update_symbol)
 
-        self.market_button.clicked.connect(self.market_monitor.showMaximized)
-        self.greeks_button.clicked.connect(self.greeks_monitor.showMaximized)
-        self.manual_button.clicked.connect(self.manual_trader.show)
-        self.chain_button.clicked.connect(self.calculate_underlying_adjustment)
+        # 创建Dock
+        self.menu_bar = self.menuBar()
+        trading_menu = self.menu_bar.addMenu("交易")
+        analysis_menu = self.menu_bar.addMenu("分析")
 
-        for button in [
-            self.market_button,
-            self.greeks_button,
-            self.chain_button,
-            self.manual_button
-        ]:
-            button.setEnabled(True)
+        market_dock = self.create_dock(
+            self.market_monitor, "T型报价", QtCore.Qt.TopDockWidgetArea
+        )
+        market_dock.setMinimumHeight(300)
+        trading_menu.addAction(market_dock.toggleViewAction())
 
-    def calculate_underlying_adjustment(self):
+        greeks_docks = self.create_dock(
+            self.greeks_monitor, "希腊值风险", QtCore.Qt.BottomDockWidgetArea
+        )
+        trading_menu.addAction(greeks_docks.toggleViewAction())
+
+        adjustment_dock = self.create_dock(
+            self.chain_monitor, "标的升贴水", QtCore.Qt.BottomDockWidgetArea
+        )
+        adjustment_dock.setMinimumWidth(300)
+        trading_menu.addAction(adjustment_dock.toggleViewAction())
+
+        hedge_dock = self.create_dock(
+            self.hedge_widget, "Delta对冲", QtCore.Qt.BottomDockWidgetArea
+        )
+        hedge_dock.setMinimumWidth(80)
+        trading_menu.addAction(hedge_dock.toggleViewAction())
+
+        manual_dock = self.create_dock(
+            self.manual_trader, "委托交易", QtCore.Qt.BottomDockWidgetArea
+        )
+        manual_dock.setMinimumWidth(250)
+        trading_menu.addAction(manual_dock.toggleViewAction())
+
+        volatility_dock = self.create_dock(
+            self.volatility_chart, "隐含波动率", QtCore.Qt.RightDockWidgetArea
+        )
+        analysis_menu.addAction(volatility_dock.toggleViewAction())
+
+        order_dock = self.create_dock(
+            self.order_monitor, "当前委托", QtCore.Qt.TopDockWidgetArea
+        )
+        trading_menu.addAction(order_dock.toggleViewAction())
+
+        trade_dock = self.create_dock(
+            self.trade_monitor, "成交", QtCore.Qt.LeftDockWidgetArea
+        )
+        trading_menu.addAction(trade_dock.toggleViewAction())
+
+        scenario_dock = self.create_dock(
+            self.scenario_chart, "情景分析", QtCore.Qt.BottomDockWidgetArea
+        )
+        analysis_menu.addAction(scenario_dock.toggleViewAction())
+
+        self.tabifyDockWidget(trade_dock, order_dock)
+
+        self.save_window_setting("default")
+
+    def create_dock(
+        self, widget: QtWidgets.QWidget, name: str, area: int
+    ):
+        """
+        Initialize a dock widget.
+        """
+        dock = QtWidgets.QDockWidget(name)
+        dock.setWidget(widget)
+        dock.setObjectName(name)
+        # dock.setFeatures(dock.DockWidgetFloatable | dock.DockWidgetMovable |)
+
+        self.docks.append(dock)
+        self.addDockWidget(area, dock)
+
+        return dock
+
+    def show(self):
         """"""
-        portfolio = self.option_engine.get_portfolio(self.portfolio_name)
+        self.showMaximized()
 
-        for chain in portfolio.chains.values():
-            chain.calculate_underlying_adjustment()
+    def closeEvent(self, event: QtGui.QCloseEvent):
+        """"""
+        self.save_window_setting("custom")
+
+        event.accept()
+
+    def save_window_setting(self, name: str):
+        """
+        Save current window size and state by trader path and setting name.
+        """
+        settings = QtCore.QSettings("Option Master", name)
+        settings.setValue("state", self.saveState())
+        settings.setValue("geometry", self.saveGeometry())
+
+    def load_window_setting(self, name: str):
+        """
+        Load previous window size and state by trader path and setting name.
+        """
+        settings = QtCore.QSettings("Option Master", name)
+        state = settings.value("state")
+        geometry = settings.value("geometry")
+
+        if isinstance(state, QtCore.QByteArray):
+            self.restoreState(state)
+            self.restoreGeometry(geometry)
+
+    def restore_window_setting(self):
+        """
+        Restore window to default setting.
+        """
+        self.load_window_setting("default")
+
+        for dock in self.docks:
+            dock.show()
+
+        self.showMaximized()
 
 
 class PortfolioDialog(QtWidgets.QDialog):
@@ -529,3 +632,126 @@ class OptionManualTrader(QtWidgets.QWidget):
         self.bv5_label.setText("-")
         self.ap5_label.setText("-")
         self.av5_label.setText("-")
+
+
+class OptionHedgeWidget(QtWidgets.QWidget):
+    """"""
+
+    def __init__(self, option_engine: OptionEngine, portfolio_name: str):
+        """"""
+        super().__init__()
+
+        self.option_engine = option_engine
+        self.portfolio_name = portfolio_name
+        self.hedge_engine = option_engine.hedge_engine
+
+        self.symbol_map: Dict[str, str] = {}
+
+        self.init_ui()
+
+    def init_ui(self):
+        """"""
+        self.setWindowTitle("Delta对冲")
+
+        underlying_symbols = []
+        portfolio = self.option_engine.get_portfolio(self.portfolio_name)
+
+        for chain in portfolio.chains.values():
+            underlying_symbol = chain.underlying.symbol
+            self.symbol_map[underlying_symbol] = chain.underlying.vt_symbol
+
+            if underlying_symbol not in underlying_symbols:
+                underlying_symbols.append(underlying_symbol)
+
+        underlying_symbols.sort()
+
+        self.symbol_combo = QtWidgets.QComboBox()
+        self.symbol_combo.addItems(underlying_symbols)
+
+        self.trigger_spin = QtWidgets.QSpinBox()
+        self.trigger_spin.setSuffix("秒")
+        self.trigger_spin.setMinimum(1)
+        self.trigger_spin.setValue(5)
+
+        self.target_spin = QtWidgets.QSpinBox()
+        self.target_spin.setMaximum(99999999)
+        self.target_spin.setMinimum(-99999999)
+        self.target_spin.setValue(0)
+
+        self.range_spin = QtWidgets.QSpinBox()
+        self.range_spin.setMinimum(0)
+        self.range_spin.setMaximum(9999999)
+        self.range_spin.setValue(12000)
+
+        self.payup_spin = QtWidgets.QSpinBox()
+        self.payup_spin.setMinimum(0)
+        self.payup_spin.setValue(3)
+
+        self.start_button = QtWidgets.QPushButton("启动")
+        self.start_button.clicked.connect(self.start)
+
+        self.stop_button = QtWidgets.QPushButton("停止")
+        self.stop_button.clicked.connect(self.stop)
+        self.stop_button.setEnabled(False)
+
+        form = QtWidgets.QFormLayout()
+        form.addRow("对冲合约", self.symbol_combo)
+        form.addRow("执行频率", self.trigger_spin)
+        form.addRow("Delta目标", self.target_spin)
+        form.addRow("对冲阈值", self.range_spin)
+        form.addRow("委托超价", self.payup_spin)
+        form.addRow(self.start_button)
+        form.addRow(self.stop_button)
+
+        self.setLayout(form)
+
+    def start(self):
+        """"""
+        symbol = self.symbol_combo.currentText()
+        vt_symbol = self.symbol_map[symbol]
+        timer_trigger = self.trigger_spin.value()
+        delta_target = self.target_spin.value()
+        delta_range = self.range_spin.value()
+        hedge_payup = self.payup_spin.value()
+
+        # Check delta of underlying
+        underlying = self.option_engine.get_instrument(vt_symbol)
+        min_range = int(underlying.theo_delta * 0.6)
+        if delta_range < min_range:
+            msg = f"Delta对冲阈值({delta_range})低于对冲合约"\
+                f"Delta值的60%({min_range})，可能导致来回频繁对冲！"
+
+            QtWidgets.QMessageBox.warning(
+                self,
+                "无法启动自动对冲",
+                msg,
+                QtWidgets.QMessageBox.Ok
+            )
+            return
+
+        self.hedge_engine.start(
+            self.portfolio_name,
+            vt_symbol,
+            timer_trigger,
+            delta_target,
+            delta_range,
+            hedge_payup
+        )
+
+        self.update_widget_status(False)
+
+    def stop(self):
+        """"""
+        self.hedge_engine.stop()
+
+        self.update_widget_status(True)
+
+    def update_widget_status(self, status: bool):
+        """"""
+        self.start_button.setEnabled(status)
+        self.symbol_combo.setEnabled(status)
+        self.target_spin.setEnabled(status)
+        self.range_spin.setEnabled(status)
+        self.payup_spin.setEnabled(status)
+        self.trigger_spin.setEnabled(status)
+        self.stop_button.setEnabled(not status)
