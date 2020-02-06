fce3660f8b6ee58a07cf4e86fa73b7d70dae681f
KeKe <45591222+1122455801@users.noreply.github.com>
2020-02-07 00:02:26 +0800

    Update generate_struct.py

--- vnpy/api/sgit/generator/generate_struct.py
+++ vnpy/api/sgit/generator/generate_struct.py
@@ -1,4 +1,5 @@
 """"""
+from typing import Dict
 import importlib
 
 
@@ -7,13 +8,13 @@ class StructGenerator:
 
     def __init__(self, filename: str, prefix: str):
         """Constructor"""
-        self.filename = filename
-        self.prefix = prefix
-        self.typedefs = {}
+        self.filename: str = filename
+        self.prefix: str = prefix
+        self.typedefs: Dict[str, str] = {}
 
         self.load_constant()
 
-    def load_constant(self):
+    def load_constant(self) -> None:
         """"""
         module_name = f"{self.prefix}_typedef"
         module = importlib.import_module(module_name)
@@ -35,7 +36,7 @@ class StructGenerator:
 
         print("Struct生成成功")
 
-    def process_line(self, line: str):
+    def process_line(self, line: str) -> None:
         """处理每行"""
         line = line.replace(";", "")
         line = line.replace("\n", "")
@@ -46,10 +47,10 @@ class StructGenerator:
             self.process_start(line)
         elif line.startswith("}"):
             self.process_end(line)
-        elif "\t" in line and "///" not in line:
+        elif "\t" in line and "//" not in line:
             self.process_member(line)
 
-    def process_declare(self, line: str):
+    def process_declare(self, line: str) -> None:
         """处理声明"""
         words = line.split(" ")
         name = words[1]
@@ -58,27 +59,33 @@ class StructGenerator:
         new_line = f"{name} = {end}\n"
         self.f_struct.write(new_line)
 
-    def process_start(self, line: str):
+    def process_start(self, line: str) -> None:
         """处理开始"""
         pass
 
-    def process_end(self, line: str):
+    def process_end(self, line: str) -> None:
         """处理结束"""
         new_line = "}\n\n"
         self.f_struct.write(new_line)
 
-    def process_member(self, line: str):
+    def process_member(self, line: str) -> None:
         """处理成员"""
         words = line.split("\t")
         words = [word for word in words if word]
 
-        py_type = self.typedefs[words[0]]
-        name = words[1]
+        word = words[0]
+        if " " in word:
+            py_type = self.typedefs[word.split(" ")[0]]
+            name = word.split(" ")[1]
+        else:
+
+            py_type = self.typedefs[word]
+            name = words[1]
 
         new_line = f"    \"{name}\": \"{py_type}\",\n"
         self.f_struct.write(new_line)
 
 
 if __name__ == "__main__":
-    generator = StructGenerator("../include/ctp/ThostFtdcUserApiStruct.h", "ctp")
+    generator = StructGenerator("../include/sgit/SgitFtdcUserApiStruct.h", "sgit")
     generator.run()
