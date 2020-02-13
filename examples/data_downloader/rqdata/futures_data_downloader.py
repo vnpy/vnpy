d86ca8b8920912995e58ad1b41294f046abb1f1a
shouhengzhang <shou-heng@hotmail.com>
2020-02-14 00:04:54 +0800

    新增批量下载rqdata数据的功能，新增批量下载tushare股票日线数据的功能

---
+++ examples/data_downloader/rqdata/futures_data_downloader.py
@@ -0,0 +1,93 @@
+"""
+自动从cf_list.csv的品种里下载数据并且保存到数据库，配合任务计划可以实现每日无人值守自动下载数据。
+代码贡献者： Shouheng Zhang
+Ver 1.0
+"""
+
+from datetime import datetime
+from vnpy.trader.constant import Interval, Exchange
+from vnpy.trader.utility import extract_vt_symbol
+from vnpy.trader.object import HistoryRequest
+from vnpy.trader.rqdata import rqdata_client
+from vnpy.trader.database import database_manager
+import csv
+import rqdatac as rq
+
+
+class RQDataDownload:
+
+    def __init__(self):
+        self.vt_symbol = ""
+        self.interval = ""
+        self.start = None
+        self.end = None
+
+        # change here to True if you want to download full history instead of download previous trading day only
+        self.download_full = False
+
+    def prep_download(self):
+        # self.start = datetime(2011, 1, 1)
+
+        self.end = datetime.now().date()
+        # read csv commodities futures list
+        with open('cf_list.csv', 'r') as f:
+            reader = csv.reader(f)
+            cf_list = list(reader)
+
+        rq.init()
+        today = rq.get_latest_trading_date()
+        prev_trading_day = rq.get_previous_trading_date(today, 1, market='cn')
+
+        for instrument in cf_list:
+            # check if changed the dominant contract for the latest trading day
+            today_dominant = rq.futures.get_dominant(instrument[1], today)
+            prev_day_dominant = rq.futures.get_dominant(instrument[1], prev_trading_day)
+            if today_dominant[today] == prev_day_dominant[prev_trading_day]:
+                self.start = prev_trading_day
+            elif today_dominant[today] != prev_day_dominant[prev_trading_day]:
+                self.start = datetime(2011, 1, 1)
+                print(f"品种 {instrument[1]} 的主力合约即日起从 {prev_day_dominant[prev_trading_day]} 换到 {today_dominant[today]}")
+            else:
+                print("####### 检查主力合约过程中出错，请手动debug！")
+            if self.download_full == True:
+                self.start = datetime(2011, 1, 1)
+            self.rq_download(instrument[0], '1m', self.start, self.end)
+            self.rq_download(instrument[0], 'd', self.start, self.end)
+
+    def rq_download(
+            self,
+            vt_symbol: str,
+            interval: str,
+            start: datetime,
+            end: datetime,
+    ):
+        rqdata_client.init()
+        symbol, exchange = extract_vt_symbol(vt_symbol)
+
+        req = HistoryRequest(
+            symbol=symbol,
+            exchange=exchange,
+            interval=Interval(interval),
+            start=start,
+            end=end
+        )
+
+        # print(req)
+
+        data = rqdata_client.query_history(req)
+
+        if data:
+            database_manager.save_bar_data(data)
+            print(f"{vt_symbol}-{interval} 历史数据下载完成")
+        else:
+            print(f"数据下载失败，无法得到 {vt_symbol} 的数据")
+
+        # print(df)
+        # print(data)
+
+if __name__ == "__main__":
+    # d1 = datetime(2019, 9, 1)
+    # d2 = datetime(2019, 9, 10)
+    a = RQDataDownload()
+    a.prep_download()
+    # a.rq_download('i888.DCE', '1m', d1, d2)
---
+++ examples/data_downloader/tushare/stock_data_downloader.py
@@ -0,0 +1,90 @@
+"""
+使用tushare接口自动下载股票的日线数据并保存到数据库，股票池可以自行定义，股票池文件为stock_pool.csv。
+起始日期可以自行修改。
+配合任务计划可以实现每日无人值守自动下载数据。
+代码贡献者：Shouheng Zhang
+Ver 1.0
+"""
+
+import tushare as ts
+import os
+import csv
+from datetime import datetime
+import time
+from vnpy.app.csv_loader import CsvLoaderEngine
+from vnpy.trader.constant import Exchange, Interval
+
+# initial parameters
+startDate = '20140101'
+endDate = datetime.now().strftime("%Y%m%d")
+
+pro = ts.pro_api()
+
+# read csv stock pool
+with open('stock_pool.csv', 'r') as f:
+    reader = csv.reader(f)
+    stockList = list(reader)
+
+# download
+for instrument in stockList:
+    print(instrument[0])
+    i = 0
+    while i < 5:
+        print(f"尝试第{i+1}次下载数据")
+        if os.path.exists("./stock_data/%s.csv" % instrument[0]):
+            currTime = time.time()
+            lastModifiedTime = os.path.getmtime("./stock_data/%s.csv" % instrument[0])
+            if os.path.getsize("./stock_data/%s.csv" % instrument[0]) / 1024 < 100 or \
+                    currTime - lastModifiedTime > 120 * 60:
+                df = ts.pro_bar(ts_code=instrument[0], adj='qfq', start_date=startDate, end_date=endDate)
+                df.to_csv('./stock_data/%s.csv' % instrument[0])
+                print(df)
+            else:
+                print(f"股票 {instrument[0]} 数据下载完成")
+                break
+        else:
+            df = ts.pro_bar(ts_code=instrument[0], adj='qfq', start_date=startDate, end_date=endDate)
+            df.to_csv('./stock_data/%s.csv' % instrument[0])
+            print(df)
+
+        i = i + 1
+
+# check
+
+for instrument in stockList:
+    if os.path.getsize("./stock_data/%s.csv" % instrument[0]) / 1024 < 100:
+        print(f'数据 {instrument[0]} 错误！文件大小太小，可能是空文件。')
+    lastModifiedTime = os.path.getmtime("./stock_data/%s.csv" % instrument[0])
+    currTime = time.time()
+    if currTime - lastModifiedTime > 40 * 60:
+        print(f'数据 {instrument[0]} 错误！今日文件未改变，请手动检查。')
+
+# save data to database use csv loader
+engine = CsvLoaderEngine(None, None)
+
+with open('stock_pool.csv', 'r') as f:
+    reader = csv.reader(f)
+    stockList = list(reader)
+
+for instrument in stockList:
+    f = ("./stock_data/%s.csv" % instrument[0])
+    if instrument[0][-2:] == 'SH':
+        temp_exchange = Exchange.SSE
+    elif instrument[0][-2:] == 'SZ':
+        temp_exchange = Exchange.SZSE
+    else:
+        print(f'股票{instrument[0]}的交易所填写错误，请手动终止程序，并检查股票池文件！')
+        os.system("pause")
+    engine.load(
+        f,
+        symbol=instrument[0][:-3],
+        exchange=temp_exchange,
+        interval=Interval.DAILY,
+        datetime_head="trade_date",
+        open_head="open",
+        close_head="close",
+        low_head="low",
+        high_head="high",
+        volume_head="vol",
+        datetime_format="%Y%m%d",
+    )
