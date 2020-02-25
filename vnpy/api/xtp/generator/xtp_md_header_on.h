9666e5c092016141386d91659a8b70ed294d8889
KeKe <45591222+1122455801@users.noreply.github.com>
2020-02-25 20:36:42 +0800

    Update xtp_md_header_on.h

--- vnpy/api/xtp/generator/xtp_md_header_on.h
+++ vnpy/api/xtp/generator/xtp_md_header_on.h
@@ -6,45 +6,45 @@ virtual void onSubMarketData(const dict &data, const dict &error, bool last) {};
 
 virtual void onUnSubMarketData(const dict &data, const dict &error, bool last) {};
 
-virtual void onDepthMarketData(const dict &data, int reqid, int reqid, int reqid, int reqid, int reqid, int reqid) {};
+virtual void onDepthMarketData(const dict &data) {};
 
 virtual void onSubOrderBook(const dict &data, const dict &error, bool last) {};
 
 virtual void onUnSubOrderBook(const dict &data, const dict &error, bool last) {};
 
-virtual void onOrderBook(const dict &data) {};
+virtual void onOrderBook() {};
 
 virtual void onSubTickByTick(const dict &data, const dict &error, bool last) {};
 
 virtual void onUnSubTickByTick(const dict &data, const dict &error, bool last) {};
 
-virtual void onTickByTick(const dict &data) {};
+virtual void onTickByTick() {};
 
-virtual void onSubscribeAllMarketData(int data, const dict &error) {};
+virtual void onSubscribeAllMarketData(int extra, const dict &error) {};
 
-virtual void onUnSubscribeAllMarketData(int data, const dict &error) {};
+virtual void onUnSubscribeAllMarketData(int extra, const dict &error) {};
 
-virtual void onSubscribeAllOrderBook(int data, const dict &error) {};
+virtual void onSubscribeAllOrderBook(int extra, const dict &error) {};
 
-virtual void onUnSubscribeAllOrderBook(int data, const dict &error) {};
+virtual void onUnSubscribeAllOrderBook(int extra, const dict &error) {};
 
-virtual void onSubscribeAllTickByTick(int data, const dict &error) {};
+virtual void onSubscribeAllTickByTick(int extra, const dict &error) {};
 
-virtual void onUnSubscribeAllTickByTick(int data, const dict &error) {};
+virtual void onUnSubscribeAllTickByTick(int extra, const dict &error) {};
 
 virtual void onQueryAllTickers(const dict &data, const dict &error, bool last) {};
 
 virtual void onQueryTickersPriceInfo(const dict &data, const dict &error, bool last) {};
 
-virtual void onSubscribeAllOptionMarketData(int data, const dict &error) {};
+virtual void onSubscribeAllOptionMarketData(int extra, const dict &error) {};
 
-virtual void onUnSubscribeAllOptionMarketData(int data, const dict &error) {};
+virtual void onUnSubscribeAllOptionMarketData(int extra, const dict &error) {};
 
-virtual void onSubscribeAllOptionOrderBook(int data, const dict &error) {};
+virtual void onSubscribeAllOptionOrderBook(int extra, const dict &error) {};
 
-virtual void onUnSubscribeAllOptionOrderBook(int data, const dict &error) {};
+virtual void onUnSubscribeAllOptionOrderBook(int extra, const dict &error) {};
 
-virtual void onSubscribeAllOptionTickByTick(int data, const dict &error) {};
+virtual void onSubscribeAllOptionTickByTick(int extra, const dict &error) {};
 
-virtual void onUnSubscribeAllOptionTickByTick(int data, const dict &error) {};
+virtual void onUnSubscribeAllOptionTickByTick(int extra, const dict &error) {};
 
