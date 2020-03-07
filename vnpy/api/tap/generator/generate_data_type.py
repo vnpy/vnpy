2c2f1af4d5004ef71699690c4cb9cb37f1501e18
KeKe <45591222+1122455801@users.noreply.github.com>
2020-03-07 18:13:12 +0800

    Update generate_data_type.py

--- vnpy/api/tap/generator/generate_data_type.py
+++ vnpy/api/tap/generator/generate_data_type.py
@@ -1,76 +1,129 @@
 """"""
 
-TYPE_CPP2PY = {
-    "int": "int",
-    "char": "char",
-    "double": "double",
-    "short": "int",
-}
-
 
 class DataTypeGenerator:
     """DataType生成器"""
 
-    def __init__(self, filename: str, prefix: str):
+    def __init__(self, filename: str, prefix: str, name: str) -> None:
         """Constructor"""
-        self.filename = filename
-        self.prefix = prefix
+        self.filename: str = filename
+        self.prefix: str = prefix
+        self.name: str = name
 
-    def run(self):
+    def run(self) -> None:
         """主函数"""
-        self.f_cpp = open(self.filename, "r")
-        self.f_define = open(f"{self.prefix}_constant.py", "w")
-        self.f_typedef = open(f"{self.prefix}_typedef.py", "w")
+        self.f_cpp = open(self.filename, "r", encoding="UTF-8")
+        self.f_define = open(f"{self.prefix}_{self.name}_constant.py", "w", encoding="UTF-8")
+        self.f_typedef = open(f"{self.prefix}_{self.name}_typedef.py", "w", encoding="UTF-8")
+        self.f_struct = open(f"{self.prefix}_{self.name}_typedef_struct.py", "w", encoding="UTF-8")
 
         for line in self.f_cpp:
             self.process_line(line)
 
+        self.f_typedef.write("TAPIINT32 = \"int\"\n")
+        self.f_typedef.write("TAPIUINT32 = \"unsigned int\"\n")
+        self.f_typedef.write("TAPIINT64 = \"long long\"\n")
+        self.f_typedef.write("TAPIUINT64 = \"unsigned long long\"\n")
+        self.f_typedef.write("TAPIUINT16 = \"unsigned short\"\n")
+        self.f_typedef.write("TAPIUINT8 = \"unsigned char\"\n")
+        self.f_typedef.write("TAPIREAL64 = \"double\"\n")
+
+        if self.name == "md":
+            self.f_typedef.write("TAPIYNFLAG = \"char\"\n")
+            self.f_typedef.write("TAPILOGLEVEL = \"char\"\n")
+            self.f_typedef.write("TAPICommodityType = \"char\"\n")
+            self.f_typedef.write("TAPICallOrPutFlagType = \"char\"\n")
+
+            self.f_typedef.write("TAPIMACTYPE = \"string\"\n")
+            self.f_typedef.write("TAPISecondSerialIDType = \"string\"\n")
+            self.f_typedef.write("TAPIClientIDType = \"string\"\n")
+
+        elif self.name == "td":
+            self.f_typedef.write("TAPIYNFLAG = \"char\"\n")
+            self.f_typedef.write("TAPIPasswordType = \"char\"\n")
+            self.f_typedef.write("TAPILOGLEVEL = \"char\"\n")
+            self.f_typedef.write("TAPIOptionType = \"char\"\n")
+            self.f_typedef.write("TAPICommodityType = \"char\"\n")
+            self.f_typedef.write("TAPICallOrPutFlagType = \"char\"\n")
+
         self.f_cpp.close()
         self.f_define.close()
         self.f_typedef.close()
+        self.f_struct.close()
 
-        print("DataType生成完毕")
+        print(f"{self.name}_DataType生成完毕")
 
-    def process_line(self, line: str):
+    def process_line(self, line: str) -> None:
         """处理每行"""
         line = line.replace("\n", "")
         line = line.replace(";", "")
 
-        if line.startswith("#define"):
-            self.process_define(line)
-        elif line.startswith("typedef"):
-            self.process_typedef(line)
+        # MD
+        if self.name == "md":
+            if line.startswith("typedef char"):
+                self.process_char_md(line)
+            elif line.startswith("const"):
+                self.process_const_md(line)
+        # TD
+        elif self.name == "td":
+            if line.startswith("    typedef char"):
+                self.process_char_td(line)
+            elif line.startswith("    const"):
+                self.process_const_td(line)
 
-    def process_define(self, line: str):
-        """处理常量定义"""
+    def process_char_md(self, line: str) -> None:
+        """处理类型定义"""
+        if "\t\t\t\t" in line:
+            name = line.split("\t\t\t\t")[1]
+            new_line = f"{name} = \"char\"\n"
+        else:
+            name = line.split("\t")[1].split("[")[0]
+            new_line = f"{name} = \"string\"\n"
+
+        self.f_typedef.write(new_line)
+
+    def process_char_td(self, line: str) -> None:
         words = line.split(" ")
-        words = [word for word in words if word]
-        if len(words) < 3:
-            return
+        words = [word for word in words if word != ""]
 
-        name = words[1]
-        value = words[2]
+        name = words[-1]
+
+        if "[" in name:
+            name = name.split("[")[0]
+            new_line = f"{name} = \"string\"\n"
+        else:
+            new_line = f"{name} = \"char\"\n"
+
+        self.f_typedef.write(new_line)
+
+    def process_const_md(self, line: str) -> None:
+        """"""
+        sectors = line.split("=")
+        value = sectors[1].replace("\'", "\"").strip()
+
+        words = sectors[0].split("\t")
+        words = [word for word in words if word != ""]
+        name = words[1].strip()
 
         new_line = f"{name} = {value}\n"
         self.f_define.write(new_line)
 
-    def process_typedef(self, line: str):
-        """处理类型定义"""
-        words = line.split(" ")
-        words = [word for word in words if word != " "]
+    def process_const_td(self, line: str):
+        sectors = line.split("=")
+        value = sectors[1].replace("\'", "\"").strip()
 
-        name = words[2]
-        typedef = TYPE_CPP2PY[words[1]]
+        words = sectors[0].split(" ")
+        words = [word for word in words if word != ""]
 
-        if typedef == "char":
-            if "[" in name:
-                typedef = "string"
-                name = name[:name.index("[")]
+        name = words[-1].strip()
 
-        new_line = f"{name} = \"{typedef}\"\n"
-        self.f_typedef.write(new_line)
+        new_line = f"{name} = {value}\n"
+        self.f_define.write(new_line)
 
 
 if __name__ == "__main__":
-    generator = DataTypeGenerator("../include/ctp/ThostFtdcUserApiDataType.h", "ctp")
-    generator.run()
+    # md_generator = DataTypeGenerator("../include/tap/TapAPICommDef.h", "tap", "md")
+    # md_generator.run()
+
+    td_generator = DataTypeGenerator("../include/tap/iTapAPICommDef.h", "tap", "td")
+    td_generator.run()
