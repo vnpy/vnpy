3ba0854ee3bdbe625be7ecb3c8ee722b513c0401
KeKe <45591222+1122455801@users.noreply.github.com>
2020-03-09 17:46:15 +0800

    Create tap_md_header_process.h

---
+++ vnpy/api/tap/generator/tap_md_header_process.h
@@ -0,0 +1,16 @@
+void processRspLogin(Task *task);
+
+void processAPIReady(Task *task);
+
+void processDisconnect(Task *task);
+
+void processRspQryCommodity(Task *task);
+
+void processRspQryContract(Task *task);
+
+void processRspSubscribeQuote(Task *task);
+
+void processRspUnSubscribeQuote(Task *task);
+
+void processRtnQuote(Task *task);
+
