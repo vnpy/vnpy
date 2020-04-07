90127c8e0f18b4fd4a7152e1d21bc50bd44a6f8d
vn.py <xiaoyou.chen@foxmail.com>
2020-04-07 12:26:00 +0800

    [Fix] bug when searching strategies folder with none python file

--- vnpy/app/spread_trading/engine.py
+++ vnpy/app/spread_trading/engine.py
@@ -638,11 +638,11 @@ class SpreadStrategyEngine:
                 if filename.endswith(".py"):
                     strategy_module_name = ".".join(
                         [module_name, filename.replace(".py", "")])
+                    self.load_strategy_class_from_module(strategy_module_name)
                 elif filename.endswith(".pyd"):
                     strategy_module_name = ".".join(
                         [module_name, filename.split(".")[0]])
-
-                self.load_strategy_class_from_module(strategy_module_name)
+                    self.load_strategy_class_from_module(strategy_module_name)
 
     def load_strategy_class_from_module(self, module_name: str):
         """
