dbbbcff7ca33225e463463be96329deb441b85a2
vn.py <xiaoyou.chen@foxmail.com>
2020-06-19 10:24:26 +0800

    [Fix] close #2539

--- vnpy/app/cta_strategy/backtesting.py
+++ vnpy/app/cta_strategy/backtesting.py
@@ -271,7 +271,7 @@ class BacktestingEngine:
         self.strategy.on_init()
 
         # Use the first [days] of history data for initializing strategy
-        day_count = 0
+        day_count = 1
         ix = 0
 
         for ix, data in enumerate(self.history_data):
