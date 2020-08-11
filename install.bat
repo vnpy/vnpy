f90cf5102b810b943164e48f3bd4413b6bfd779b
vn.py <xiaoyou.chen@foxmail.com>
2020-08-11 20:48:05 +0800

    [Add] quickfix dependency

--- install.bat
+++ install.bat
@@ -1,8 +1,9 @@
 :: Upgrade pip & setuptools
 python -m pip install --upgrade pip setuptools
 
-::Install talib and ibapi
+::Install prebuild wheel
 python -m pip install https://pip.vnpy.com/colletion/TA_Lib-0.4.17-cp37-cp37m-win_amd64.whl
+python -m pip install https://pip.vnpy.com/colletion/quickfix-1.15.1-cp37-cp37m-win_amd64.whl
 
 ::Install Python Modules
 python -m pip install -r requirements.txt
