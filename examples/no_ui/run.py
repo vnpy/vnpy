0f30e86cf7dfa9d83f8e151647df621ab6e070ee
vn.py <xiaoyou.chen@foxmail.com>
2020-08-22 14:31:15 +0800

    [Mod] save cta strategy data when closing child process

--- examples/no_ui/run.py
+++ examples/no_ui/run.py
@@ -1,4 +1,5 @@
 import multiprocessing
+import sys
 from time import sleep
 from datetime import datetime, time
 from logging import INFO
@@ -29,6 +30,29 @@ ctp_setting = {
 }
 
 
+# Chinese futures market trading period (day/night)
+DAY_START = time(8, 45)
+DAY_END = time(14, 29)
+
+NIGHT_START = time(20, 45)
+NIGHT_END = time(2, 45)
+
+
+def check_trading_period():
+    """"""
+    current_time = datetime.now().time()
+
+    trading = False
+    if (
+        (current_time >= DAY_START and current_time <= DAY_END)
+        or (current_time >= NIGHT_START)
+        or (current_time <= NIGHT_END)
+    ):
+        trading = True
+
+    return trading
+
+
 def run_child():
     """
     Running in the child process.
@@ -48,13 +72,13 @@ def run_child():
     main_engine.connect(ctp_setting, "CTP")
     main_engine.write_log("连接CTP接口")
 
-    sleep(10)
+    sleep(1)
 
     cta_engine.init_engine()
     main_engine.write_log("CTA策略初始化完成")
 
     cta_engine.init_all_strategies()
-    sleep(60)   # Leave enough time to complete strategy initialization
+    sleep(6)   # Leave enough time to complete strategy initialization
     main_engine.write_log("CTA策略全部初始化")
 
     cta_engine.start_all_strategies()
@@ -63,6 +87,12 @@ def run_child():
     while True:
         sleep(1)
 
+        trading = check_trading_period()
+        if not trading:
+            print("关闭子进程")
+            main_engine.close()
+            sys.exit(0)
+
 
 def run_parent():
     """
@@ -70,26 +100,10 @@ def run_parent():
     """
     print("启动CTA策略守护父进程")
 
-    # Chinese futures market trading period (day/night)
-    DAY_START = time(8, 45)
-    DAY_END = time(15, 30)
-
-    NIGHT_START = time(20, 45)
-    NIGHT_END = time(2, 45)
-
     child_process = None
 
     while True:
-        current_time = datetime.now().time()
-        trading = False
-
-        # Check whether in trading period
-        if (
-            (current_time >= DAY_START and current_time <= DAY_END)
-            or (current_time >= NIGHT_START)
-            or (current_time <= NIGHT_END)
-        ):
-            trading = True
+        trading = check_trading_period()
 
         # Start child process in trading period
         if trading and child_process is None:
@@ -100,11 +114,9 @@ def run_parent():
 
         # 非记录时间则退出子进程
         if not trading and child_process is not None:
-            print("关闭子进程")
-            child_process.terminate()
-            child_process.join()
-            child_process = None
-            print("子进程关闭成功")
+            if not child_process.is_alive():
+                child_process = None
+                print("子进程关闭成功")
 
         sleep(5)
 
