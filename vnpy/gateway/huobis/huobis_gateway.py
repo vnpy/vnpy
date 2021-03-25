cd9aca99d2558c29f6d1beaba0f7bc30b3aa64a2
vn.py <xiaoyou.chen@foxmail.com>
2021-03-25 21:30:52 +0800

    [Mod] use contract_size directly

--- vnpy/gateway/huobis/huobis_gateway.py
+++ vnpy/gateway/huobis/huobis_gateway.py
@@ -655,17 +655,12 @@ class HuobisRestApi(RestClient):
             if not self.usdt_base or d["support_margin_mode"] != "isolated":
                 self.contract_codes.add(d["contract_code"])
 
-                if "." in d["contract_size"]:
-                    size = float(d["contract_size"])
-                else:
-                    size = int(d["contract_size"])
-
                 contract = ContractData(
                     symbol=d["contract_code"],
                     exchange=Exchange.HUOBI,
                     name=d["contract_code"],
                     pricetick=d["price_tick"],
-                    size=size,
+                    size=d["contract_size"],
                     min_volume=1,
                     product=Product.FUTURES,
                     history_data=True,
