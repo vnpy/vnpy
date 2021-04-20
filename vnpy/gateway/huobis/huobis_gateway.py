14db625583aa2a20e949a6b84dbf45581b1982e8
noranhe <63988546+noranhe@users.noreply.github.com>
2021-04-20 09:51:38 +0800

    Update huobis_gateway.py

--- vnpy/gateway/huobis/huobis_gateway.py
+++ vnpy/gateway/huobis/huobis_gateway.py
@@ -235,7 +235,8 @@ class HuobisRestApi(RestClient):
         Generate HUOBIS signature.
         """
         request.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36",
+            "Connection": "close"
         }
         params_with_signature = create_signature(
             self.key,
