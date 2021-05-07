23039be533aba1e9000b17084cb29ed1c11fb430
vn.py <xiaoyou.chen@foxmail.com>
2021-05-07 21:14:11 +0800

    [Mod] use glob to find strategy files

--- vnpy/trader/object.py
+++ vnpy/trader/object.py
@@ -70,7 +70,7 @@ class TickData(BaseData):
     ask_volume_3: float = 0
     ask_volume_4: float = 0
     ask_volume_5: float = 0
-    
+
     local_time: datetime = None
 
     def __post_init__(self):
