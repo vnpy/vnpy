82088f93b5109905fc96ec9e7ec8277c341563b2
vn.py <xiaoyou.chen@foxmail.com>
2021-09-05 11:17:10 +0800

    [Mod] user vnpy_spreadtrading for spread trading app

--- vnpy/app/spread_trading/__init__.py
+++ vnpy/app/spread_trading/__init__.py
@@ -1,30 +1,6 @@
-from pathlib import Path
+import sys
 
-from vnpy.trader.app import BaseApp
-from vnpy.trader.object import (
-    OrderData,
-    TradeData,
-    TickData,
-    BarData
-)
-
-from .engine import (
-    SpreadEngine,
-    APP_NAME,
-    SpreadData,
-    LegData,
-    SpreadStrategyTemplate,
-    SpreadAlgoTemplate
-)
+import vnpy_spreadtrading
 
 
-class SpreadTradingApp(BaseApp):
-    """"""
-
-    app_name = APP_NAME
-    app_module = __module__
-    app_path = Path(__file__).parent
-    display_name = "价差交易"
-    engine_class = SpreadEngine
-    widget_name = "SpreadManager"
-    icon_name = "spread.ico"
+sys.modules[__name__] = vnpy_spreadtrading
