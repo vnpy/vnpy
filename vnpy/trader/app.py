cd5f68fa3bab7ea40be633361552cba1b8d2e44f
vn.py <xiaoyou.chen@foxmail.com>
2022-02-17 11:18:32 +0800

    [Mod] change app_path type

--- vnpy/trader/app.py
+++ vnpy/trader/app.py
@@ -1,6 +1,7 @@
 """"""
 
 from abc import ABC
+from pathlib import Path
 
 
 class BaseApp(ABC):
@@ -8,10 +9,10 @@ class BaseApp(ABC):
     Absstract class for app.
     """
 
-    app_name: str = ""           # Unique name used for creating engine and widget
-    app_module: str = ""         # App module string used in import_module
-    app_path: str = ""           # Absolute path of app folder
-    display_name: str = ""       # Name for display on the menu.
-    engine_class = None     # App engine class
-    widget_name: str = ""        # Class name of app widget
-    icon_name: str = ""          # Icon file name of app widget
+    app_name: str = ""          # Unique name used for creating engine and widget
+    app_module: str = ""        # App module string used in import_module
+    app_path: Path = ""         # Absolute path of app folder
+    display_name: str = ""      # Name for display on the menu.
+    engine_class = None         # App engine class
+    widget_name: str = ""       # Class name of app widget
+    icon_name: str = ""         # Icon file name of app widget
