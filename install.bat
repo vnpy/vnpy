8cfd11d4ec174a56f9fc44659ac70c69adf956f4
Edanflame <237397488@qq.com>
2022-03-15 10:10:10 +0800

    [Mod] 更新安装脚本

--- install.bat
+++ install.bat
@@ -2,7 +2,10 @@
 python -m pip install --upgrade pip wheel
 
 ::Install prebuild wheel
-python -m pip install https://pip.vnpy.com/colletion/TA_Lib-0.4.17-cp37-cp37m-win_amd64.whl
+python -m pip install --extra-index-url http://139.196.190.180 --trusted-host 139.196.190.180 TA_Lib
+
+::Degrade setuptools to install deap 1.3.1 for python3.10
+python -m pip install setuptools==57.0.0
 
 ::Install Python Modules
 python -m pip install -r requirements.txt
