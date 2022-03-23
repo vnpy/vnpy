46fa0c6a51e0f71664f347b745b836de5c479c7e
fsksf <timeless.go@foxmail.com>
2022-03-23 13:04:24 +0800

    修改界面显示

--- vnpy/trader/ui/widget.py
+++ vnpy/trader/ui/widget.py
@@ -508,7 +508,7 @@ class PositionMonitor(BaseMonitor):
         "volume": {"display": "数量", "cell": BaseCell, "update": True},
         "yd_volume": {"display": "昨仓", "cell": BaseCell, "update": True},
         "sell_able": {"display": "可卖", "cell": BaseCell, "update": True},
-        "purchase_able": {"display": "可申", "cell": BaseCell, "update": True},
+        "purchase_able": {"display": "可申赎", "cell": BaseCell, "update": True},
         "frozen": {"display": "冻结", "cell": BaseCell, "update": True},
         "price": {"display": "均价", "cell": BaseCell, "update": True},
         "pnl": {"display": "盈亏", "cell": PnlCell, "update": True},
