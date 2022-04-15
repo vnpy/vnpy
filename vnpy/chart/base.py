602934364ee65bb8a5e8312c06e42ecfd374290a
noranhe <63988546+noranhe@users.noreply.github.com>
2022-04-15 18:00:34 +0800

    添加类型声明-vnpy.chart

--- vnpy/chart/base.py
+++ vnpy/chart/base.py
@@ -1,19 +1,19 @@
 from vnpy.trader.ui import QtGui
 
 
-WHITE_COLOR = (255, 255, 255)
-BLACK_COLOR = (0, 0, 0)
-GREY_COLOR = (100, 100, 100)
+WHITE_COLOR: tuple = (255, 255, 255)
+BLACK_COLOR: tuple = (0, 0, 0)
+GREY_COLOR: tuple = (100, 100, 100)
 
-UP_COLOR = (255, 75, 75)
-DOWN_COLOR = (0, 255, 255)
-CURSOR_COLOR = (255, 245, 162)
+UP_COLOR: tuple = (255, 75, 75)
+DOWN_COLOR: tuple = (0, 255, 255)
+CURSOR_COLOR: tuple = (255, 245, 162)
 
-PEN_WIDTH = 1
-BAR_WIDTH = 0.3
+PEN_WIDTH: int = 1
+BAR_WIDTH: float = 0.3
 
-AXIS_WIDTH = 0.8
-NORMAL_FONT = QtGui.QFont("Arial", 9)
+AXIS_WIDTH: float = 0.8
+NORMAL_FONT: QtGui.QFont = QtGui.QFont("Arial", 9)
 
 
 def to_int(value: float) -> int:
