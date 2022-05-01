d8fbcd28d46e9bddc7f9568999138facfb3833ed
vn.py <xiaoyou.chen@foxmail.com>
2022-05-01 14:57:12 +0800

    [Fix] use TYPE_CHECKING to avoid loop import

--- vnpy/trader/app.py
+++ vnpy/trader/app.py
@@ -1,8 +1,10 @@
 from abc import ABC
 from pathlib import Path
-from typing import Type
+from typing import Type, TYPE_CHECKING
 
-from .engine import BaseEngine
+
+if TYPE_CHECKING:
+    from .engine import BaseEngine
 
 
 class BaseApp(ABC):
@@ -10,10 +12,10 @@ class BaseApp(ABC):
     Absstract class for app.
     """
 
-    app_name: str = ""                      # Unique name used for creating engine and widget
-    app_module: str = ""                    # App module string used in import_module
-    app_path: Path = ""                     # Absolute path of app folder
-    display_name: str = ""                  # Name for display on the menu.
-    engine_class: Type[BaseEngine] = None   # App engine class
-    widget_name: str = ""                   # Class name of app widget
-    icon_name: str = ""                     # Icon file name of app widget
+    app_name: str = ""                          # Unique name used for creating engine and widget
+    app_module: str = ""                        # App module string used in import_module
+    app_path: Path = ""                         # Absolute path of app folder
+    display_name: str = ""                      # Name for display on the menu.
+    engine_class: Type["BaseEngine"] = None     # App engine class
+    widget_name: str = ""                       # Class name of app widget
+    icon_name: str = ""                         # Icon file name of app widget
