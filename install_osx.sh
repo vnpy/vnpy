dc0a6d733d7607040b5c56a3674507630b829ce2
Edanflame <237397488@qq.com>
2022-08-12 11:01:01 +0800

    [Mod] 更新安装脚本，去除deap库前置安装要求

--- install_osx.sh
+++ install_osx.sh
@@ -23,9 +23,6 @@ ta-lib-exists || install-ta-lib
 $python -m pip install numpy==1.21.5
 $python -m pip install ta-lib==0.4.24
 
-# degrade setuptools to install deap 1.3.1 for python3.10 
-$python -m pip install setuptools==57.0.0
-
 # Install Python Modules
 $python -m pip install -r requirements.txt
 
