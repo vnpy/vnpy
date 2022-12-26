736f20f3aa2dd0f494baff21d709b40fc8f09238
noranhe <63988546+noranhe@users.noreply.github.com>
2022-12-26 18:11:17 +0800

    [Mod] update install.bat

--- install.bat
+++ install.bat
@@ -2,7 +2,8 @@
 python -m pip install --upgrade pip wheel
 
 ::Install prebuild wheel
-python -m pip install --extra-index-url http://139.196.190.180 --trusted-host 139.196.190.180 TA_Lib
+python -m pip install numpy==1.21.6
+python -m pip install --extra-index-url http://139.196.190.180 --trusted-host 139.196.190.180 TA_Lib==0.4.24
 
 ::Install Python Modules
 python -m pip install -r requirements.txt
