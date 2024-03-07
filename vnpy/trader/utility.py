18bb524f4584f6818985e975e41c643b7f43fa23
lance yuan <45159389+lance52587@users.noreply.github.com>
2024-03-07 13:33:50 +0800

    Update utility.py

--- vnpy/trader/utility.py
+++ vnpy/trader/utility.py
@@ -31,7 +31,7 @@ def extract_vt_symbol(vt_symbol: str) -> Tuple[str, Exchange]:
     :return: (symbol, exchange)
     """
     idx = vt_symbol.rfind(".")
-    symbol = vt_symbol[:]
+    symbol = vt_symbol[:idx]
     exchange_str = vt_symbol[idx+1:]
     return symbol, Exchange(exchange_str)
 
