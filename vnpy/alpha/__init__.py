4f46ff4e7120cb1c1d0821507908a5ed9b5b4648
vn.py <xiaoyou.chen@foxmail.com>
2025-03-26 15:56:16 +0800

    [Add] alpha strategy template

--- vnpy/alpha/__init__.py
+++ vnpy/alpha/__init__.py
@@ -1,12 +1,15 @@
+from .logger import logger
 from .dataset import AlphaDataset, Segment, to_datetime
 from .model import AlphaModel
-from .logger import logger
+from .strategy import AlphaStrategy, BacktestingEngine
 
 
 __all__ = [
+    "logger",
     "AlphaDataset",
     "Segment",
     "to_datetime",
     "AlphaModel",
-    "logger",
+    "AlphaStrategy",
+    "BacktestingEngine"
 ]
