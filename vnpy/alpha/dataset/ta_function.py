4b858f48d638756dffad7c33abd58e1310ae3a39
vn.py <xiaoyou.chen@foxmail.com>
2025-03-26 11:31:55 +0800

    [Add] operator functions for calculating data within alpha dataset

---
+++ vnpy/alpha/dataset/ta_function.py
@@ -0,0 +1,41 @@
+"""
+Technical Analysis Operators
+"""
+
+import talib
+import polars as pl
+import pandas as pd
+
+from .utility import DataProxy
+
+
+def to_pd_series(feature: DataProxy) -> pd.Series:
+    """Convert to pandas.Series data structure"""
+    return feature.df.to_pandas().set_index(["datetime", "vt_symbol"])["data"]
+
+
+def to_pl_dataframe(series: pd.Series) -> pl.DataFrame:
+    """Convert to polars.DataFrame data structure"""
+    return pl.from_pandas(series.reset_index().rename(columns={0: "data"}))
+
+
+def rsi(close: DataProxy, window: int) -> DataProxy:
+    """Calculate RSI indicator by contract"""
+    close_: pd.Series = to_pd_series(close)
+
+    result: pd.Series = talib.RSI(close_, timeperiod=window)   # type: ignore
+
+    df: pl.DataFrame = to_pl_dataframe(result)
+    return DataProxy(df)
+
+
+def atr(high: DataProxy, low: DataProxy, close: DataProxy, window: int) -> DataProxy:
+    """Calculate ATR indicator by contract"""
+    high_: pd.Series = to_pd_series(high)
+    low_: pd.Series = to_pd_series(low)
+    close_: pd.Series = to_pd_series(close)
+
+    result: pd.Series = talib.ATR(high_, low_, close_, timeperiod=window)   # type: ignore
+
+    df: pl.DataFrame = to_pl_dataframe(result)
+    return DataProxy(df)
