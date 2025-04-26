397a85530a40dd7be5f4611be204a5a4e7b40690
EvanHong99 <939778128@qq.com>
2025-04-26 10:49:36 +0800

    [fix] delete cached portfolio_strategy

--- vnpy/app/portfolio_strategy/locale/generate_mo.bat
+++
@@ -1,1 +0,0 @@
-python C:\veighna_studio\Tools\i18n\msgfmt.py -o vnpy_portfoliostrategy\locale\en\LC_MESSAGES\vnpy_portfoliostrategy.mo vnpy_portfoliostrategy\locale\en\LC_MESSAGES\vnpy_portfoliostrategy
